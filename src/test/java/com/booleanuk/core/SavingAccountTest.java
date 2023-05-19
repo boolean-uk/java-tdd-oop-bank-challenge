7be1c1b58078982d589056b386db0ab159902442
Farhang Younis <comp-sci@hotmail.com>
2023-05-19 12:46:38 +0200

    Added test for Withdraw method of SavingAccount - fails due to no implimentation

--- src/test/java/com/booleanuk/core/SavingAccountTest.java
+++ src/test/java/com/booleanuk/core/SavingAccountTest.java
@@ -17,4 +17,20 @@ public class SavingAccountTest {
         Assertions.assertEquals(1,account.getTransactions().size());
         Assertions.assertEquals(TransactionType.CREDIT,account.getTransactions().get(0).getType());
     }
+    @Test
+    public void testWithdraw(){
+        // the test should decrease the balance and add a transaction of type DEBIT
+        SavingAccount account = new SavingAccount(0);
+        account.deposit(100.00, LocalDate.of(2023,5,19));
+        account.deposit(100.00, LocalDate.of(2023,5,19));
+        account.deposit(100.00, LocalDate.of(2023,5,19));
+
+        Assertions.assertEquals(300.00,account.getBalance());
+        account.withdraw(100.00, LocalDate.of(2023,5,19));
+
+        Assertions.assertEquals(200.00,account.getBalance());
+
+        Assertions.assertEquals(4,account.getTransactions().size());
+        Assertions.assertEquals(TransactionType.CREDIT,account.getTransactions().get(3).getType());
+    }
 }

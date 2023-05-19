66b833418e62c53fe64287b70b3e60fc4c62ad37
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-05-19 14:56:27 +0200

    updated class-diagram.png + changed Account statement() to backwards for-loop + comment clean-up

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -2,11 +2,10 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class Account {
     final static List<Account> accounts = new ArrayList<>();
-    final Stack<ITransaction> transactions = new Stack<>();
+    final List<ITransaction> transactions = new ArrayList<>();
     final String branch;
     public boolean overdraft;
 
@@ -28,23 +27,22 @@ public class Account {
     }
 
     public void statement() {
-        Stack<ITransaction> transactions = (Stack<ITransaction>) this.transactions.clone();
-        String result = "\nStatement:" +
-                "\ndate        || deposit     || withdraw     || balance";
-        while(transactions.size() > 0) {
-            double balance = transactions.stream().reduce(0.0, (x, y) -> x + y.signedAmount(), Double::sum);
-            result += transactions.pop() + "" + balance;
+        StringBuilder result = new StringBuilder("""
+
+                Statement:
+                date        || deposit     || withdraw     || balance""");
+
+        double balance = balance();
+        for(int i = transactions.size()-1; i > -1; i--) {
+            result.append(transactions.get(i)).append(balance);
+            balance -= transactions.get(i).signedAmount();
         }
-//        for(int i = transactions.size()-1; i > -1; i--) {
-//            result += transactions.get(i);
-//        }
-        result += "\n";
+        result.append("\n");
         System.out.println(result);
     }
 
     public double balance() {
         if(this.transactions.size() < 1) return 0.0;
-//        return this.transactions.get(transactions.size() - 1).balance();
         return this.transactions.stream().reduce(0.0, (x, y) -> x + y.signedAmount(), Double::sum);
     }
 }

ea5ef5e96858338d10942d6ac2e1f2f5430738e6
Dennis <dennisvoutos@gmail.com>
2023-05-19 17:40:00 +0300

    Customer class works as expected

--- src/main/java/com/booleanuk/core/Customer.java
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -1,11 +1,14 @@
 package com.booleanuk.core;
 
 public class Customer {
-    Account account;
-    String name,address, dateOfBirth;
-    int taxpayerIdentificationNumber;
+    private Account account;
+    private String name;
+    private String address;
+    private final String dateOfBirth;
+    private int taxpayerIdentificationNumber;
 
     //being a customer in a bank requires you provide all of the attributes in this customer class
+    //You name and your address can change legally. But you cannot change your date of birth or your assigned taxpayer number, so there are no setters for these attributes.
     public Customer(String fullName, String address, String dateBirt, int taxpayerIdentificationNumber) {
         this.name = fullName;
         this.address = address;
@@ -19,7 +22,45 @@ public class Customer {
     }
     public void setUpAccount(int intPart, int decimalPart){
         System.out.println("Setting up account....");
+        try{
+            Thread.sleep(1000);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         account = new Account(intPart,decimalPart);
+
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getTaxpayerIdentificationNumber() {
+        return taxpayerIdentificationNumber;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
 }
--- src/test/java/com/booleanuk/core/TestCustomer.java
+++ src/test/java/com/booleanuk/core/TestCustomer.java
@@ -1,4 +1,23 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestCustomer {
+    @Test
+    public void testConstructor(){
+        Customer myself = new Customer("Dennis Voutos","Athens Greece","26/03/1998",12345);
+        Assertions.assertEquals("Dennis Voutos",myself.getName());
+        Assertions.assertEquals("Athens Greece",myself.getAddress());
+        Assertions.assertEquals("26/03/1998",myself.getDateOfBirth());
+        Assertions.assertEquals(12345,myself.getTaxpayerIdentificationNumber());
+    }
+    @Test
+    public void testSetUpAccount(){
+        Customer myself = new Customer("Dennis Voutos","Athens Greece","26/03/1998",12345);
+        myself.setUpAccount(new Balance(10,0));
+        Assertions.assertEquals(10,myself.getAccount().getBalance().getIntPart());
+        Customer mother = new Customer("Katsarou P.","Zante Greece", "Never say a woman's age",6789);
+        mother.setUpAccount(new Balance(-1,0)); // error message prints correctly
+    }
 }

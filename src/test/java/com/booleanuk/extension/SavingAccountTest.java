c828a12722e52dbfc00c8fd11982b1eafd6c34ec
Farhang Younis <comp-sci@hotmail.com>
2023-05-19 15:21:26 +0200

    Test for getting balance based on transactions completed

--- src/test/java/com/booleanuk/extension/SavingAccountTest.java
+++ src/test/java/com/booleanuk/extension/SavingAccountTest.java
@@ -3,12 +3,22 @@ package com.booleanuk.extension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class SavingAccountTest {
     @Test
     public void testCreateAccountWithBranch(){
         SavingAccount account = new SavingAccount(0, new Branch("123","Somewhere"));
         Assertions.assertEquals("123",account.getBranch().getCode());
         Assertions.assertEquals("Somewhere",account.getBranch().getAddress());
+    }
+    @Test
+    public void testGetBalanceBasedOnTransactionHistory(){
+        SavingAccount account = new SavingAccount(0,new Branch("xxxx","xxxx"));
+        account.deposit(1000.00, LocalDate.of(2012,1,10));
+        account.deposit(2000.00,LocalDate.of(2012,1,13));
+        account.withdraw(500.00,LocalDate.of(2012,1,14));
 
+        Assertions.assertEquals(2500,account.getBalance());
     }
 }

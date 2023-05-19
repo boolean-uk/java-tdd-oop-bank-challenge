48db22193dbcc2eb953147c1265fedf6cae84f6c
Farhang Younis <comp-sci@hotmail.com>
2023-05-19 11:29:10 +0200

    Added test for adding accounts to the bank - succeded

--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BankTest {
+    @Test
+    public void testAddAccountToTheBank(){
+        Bank bank = new Bank();
+        bank.getAccounts().add(new CurrentCount(12.00));
+        bank.getAccounts().add(new SavingAccount(10.00));
+
+        Assertions.assertEquals(2,bank.getAccounts().size());
+    }
 }

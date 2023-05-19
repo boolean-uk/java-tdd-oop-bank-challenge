ff8632fcfc4923897ba004a907aabfd25ea15e21
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-19 14:04:17 +0200

    Added tests for creating a current account

--- src/main/java/com/booleanuk/core/AccountActions.java
+++ src/main/java/com/booleanuk/core/AccountActions.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class AccountActions {
     List<Integer> transactions = new ArrayList<>();
+    List<Account> accounts = new ArrayList<>();
 
-    public AccountActions(List<Integer> transactions) {
-        this.transactions = transactions;
+    public boolean createAccount(String accountNumber, String accountHolderFirstName, String accountHolderLastName, String branchCode) {
+
+        return true;
     }
-
-
 
 }
--- src/test/java/com/booleanuk/core/TestAccountActions.java
+++ src/test/java/com/booleanuk/core/TestAccountActions.java
@@ -1,11 +1,18 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestAccountActions {
     @Test
     public void testCreateCurrentAccount() {
-        
+        AccountActions accountAction = new AccountActions();
+
+        //Test for successful creation of account
+        Assertions.assertTrue(accountAction.createAccount("NDBK-0000-0001", "Aidan", "van Geest", "ROT001"));
+
+        //Test for failed creation of account
+        Assertions.assertFalse(accountAction.createAccount("NDBK-0000-0001", "Aidan", "van Geest", "ROT001"));
     }
 
 

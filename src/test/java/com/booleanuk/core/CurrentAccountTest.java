2d49140fa7c1c96d5d8bf9629250cd201e8db3e6
Farhang Younis <comp-sci@hotmail.com>
2023-05-19 12:59:12 +0200

    Added test for withdraw of CurrentAccount - fails due to no implimentation

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -17,4 +17,19 @@ public class CurrentAccountTest {
         Assertions.assertEquals(1,account.getTransactions().size());
         Assertions.assertEquals(TransactionType.CREDIT,account.getTransactions().get(0).getType());
     }
+    @Test
+    public void testWithdraw(){
+        // the test should decrease the balance and add a transaction of type DEBIT
+        CurrentAccount account = new CurrentAccount(0);
+        account.deposit(100.00, LocalDate.of(2023,5,19));
+        account.deposit(100.00, LocalDate.of(2023,5,19));
+        account.deposit(100.00, LocalDate.of(2023,5,19));
+
+        Assertions.assertEquals(300.00,account.getBalance());
+        account.withdraw(100.00, LocalDate.of(2023,5,19));
+
+        Assertions.assertEquals(200.00,account.getBalance());
+        Assertions.assertEquals(4,account.getTransactions().size());
+        Assertions.assertEquals(TransactionType.DEBIT,account.getTransactions().get(3).getType());
+    }
 }

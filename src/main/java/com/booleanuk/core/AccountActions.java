a9d3638b2379564269375f8e3a471a65d93c8019
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-19 14:12:53 +0200

    Updated tests for creating an account

--- src/main/java/com/booleanuk/core/AccountActions.java
+++ src/main/java/com/booleanuk/core/AccountActions.java
@@ -7,7 +7,14 @@ public class AccountActions {
     List<Integer> transactions = new ArrayList<>();
     List<Account> accounts = new ArrayList<>();
 
-    public boolean createAccount(String accountNumber, String accountHolderFirstName, String accountHolderLastName, String branchCode) {
+    public boolean createAccount(String accountType, String accountNumber, String accountHolderFirstName, String accountHolderLastName, String branchCode) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                System.out.println("Could not create account because this account number is already in use");
+                return false;
+            }
+        }
+
 
         return true;
     }
--- src/test/java/com/booleanuk/core/TestAccountActions.java
+++ src/test/java/com/booleanuk/core/TestAccountActions.java
@@ -5,14 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class TestAccountActions {
     @Test
-    public void testCreateCurrentAccount() {
+    public void testCreateAccount() {
         AccountActions accountAction = new AccountActions();
 
-        //Test for successful creation of account
-        Assertions.assertTrue(accountAction.createAccount("NDBK-0000-0001", "Aidan", "van Geest", "ROT001"));
+        //Test for successful creation of current account
+        Assertions.assertTrue(accountAction.createAccount("Current","NDBK-0000-0001", "Aidan", "van Geest", "ROT001"));
 
-        //Test for failed creation of account
-        Assertions.assertFalse(accountAction.createAccount("NDBK-0000-0001", "Aidan", "van Geest", "ROT001"));
+        //Test for successful creation of savings account
+        Assertions.assertTrue(accountAction.createAccount("Savings","NDBK-0000-0002", "Aidan", "van Geest", "ROT001"));
+
+        //Test for failed creation of account due to invalid account number
+        Assertions.assertFalse(accountAction.createAccount("Current","NDBK-0000-0001", "Aidan", "van Geest", "ROT001"));
+
+        //Test for failed creation of account due to invalid account type
+        Assertions.assertFalse(accountAction.createAccount("Investment","NDBK-0000-0003", "Aidan", "van Geest", "ROT001"));
+
+
     }
 
 

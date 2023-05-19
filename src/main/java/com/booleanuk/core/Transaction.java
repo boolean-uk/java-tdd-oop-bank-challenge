d4dcfa45c4042e5bb852e4db98f7f9c5e48ec6be
brightDeanz <konstantinos.vroustouris@gmail.com>
2023-05-20 02:55:41 +0300

    added tests for deposits

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,4 +1,31 @@
 package com.booleanuk.core;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Transaction {
+
+    private Date date;
+    private BigDecimal amount;
+
+    public Transaction(Date date, double amount) {
+        this.date = date;
+        this.amount = BigDecimal.valueOf(amount);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class AccountTest {
 
@@ -34,5 +35,34 @@ public class AccountTest {
         bank.createAccount(Bank.AccountType.CURRENT, -34);
 
         Assertions.assertEquals(BigDecimal.ZERO, bank.getAccounts().get(0).getBalance());
+    }
+
+
+    @Test
+    public void shouldReturnFalseIfAccountDoesNotExist(){
+        int accountId = 100;
+
+        Assertions.assertFalse(bank.deposit(accountId, accountId));
+    }
+    @Test
+    public void shouldReturnFalseIfDepositAmountIsZeroOrNegative(){
+        double initialBalance = 2500;
+        bank.createAccount(Bank.AccountType.CURRENT, initialBalance);
+        int accountId = bank.getAccounts().get(0).getId();
+
+        Assertions.assertFalse(bank.deposit(accountId, -100));
+        Assertions.assertEquals(BigDecimal.valueOf(initialBalance), bank.getAccounts().get(accountId).getBalance());
+
+    }
+
+    @Test
+    public void shouldReturnTrueIfAccountAndDepositAmountIsValid(){
+        double initialBalance = 2500;
+        bank.createAccount(Bank.AccountType.CURRENT, initialBalance);
+        int accountId = bank.getAccounts().get(0).getId();
+        double depositAmout = 500d;
+        Assertions.assertTrue(bank.deposit(accountId, depositAmout));
+        Assertions.assertEquals(BigDecimal.valueOf(initialBalance + depositAmout), bank.getAccounts().get(accountId).getBalance());
+
     }
 }

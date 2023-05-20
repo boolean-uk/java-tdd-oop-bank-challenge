1f02d6dccd7633c1782e80d33c469c2de4656ece
Farhang Younis <comp-sci@hotmail.com>
2023-05-20 09:25:33 +0200

    made some recaftoring to testOverdraftApproval

--- src/test/java/com/booleanuk/extension/BankTest.java
+++ src/test/java/com/booleanuk/extension/BankTest.java
@@ -21,18 +21,24 @@ public class BankTest {
         // Adding accounts to the bank
         Bank bank = new Bank();
         bank.getAccounts().add(new CurrentAccount(0, new Branch("xxx", "xx")));
+        bank.getAccounts().add(new CurrentAccount(0, new Branch("xxx", "xx")));
         bank.getAccounts().add(new SavingAccount(0, new Branch("xxx", "xx")));
-        bank.getAccounts().add(new SavingAccount(0, new Branch("xxx", "xx")));
+        bank.getAccounts().add(new CurrentAccount(0, new Branch("xxx", "xx")));
 
         // Adding/making overdraft requests
         bank.requestOverdraft(bank.getAccounts().get(0));
         bank.requestOverdraft(bank.getAccounts().get(1));
 
         // Testing for approval only for CurrentAccount
-        Assertions.assertEquals("Approved", bank.overdraftApproval(bank.getAccounts().get(0)));
-        Assertions.assertEquals("Rejected", bank.overdraftApproval(bank.getAccounts().get(1)));
+        Assertions.assertEquals("Approved", bank.overdraftApproval(bank.getAccounts().get(0), true));
+        Assertions.assertEquals("Rejected", bank.overdraftApproval(bank.getAccounts().get(1), false));
+
         // Testing for bad request, for accounts that are not in the request array
-        Assertions.assertEquals("Invalid request", bank.overdraftApproval(bank.getAccounts().get(2)));
+        Assertions.assertEquals("Invalid request", bank.overdraftApproval(bank.getAccounts().get(3)));
+
+        // Testing for correct value change approved/rejected
+        Assertions.assertTrue(((CurrentAccount)bank.getAccounts().get(0)).isOverdraft());
+        Assertions.assertFalse(((CurrentAccount)bank.getAccounts().get(1)).isOverdraft());
     }
 
 }

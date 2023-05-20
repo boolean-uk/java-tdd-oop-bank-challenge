bc1d5e2222134e3d3113de6d4848a0fc520d2343
Farhang Younis <comp-sci@hotmail.com>
2023-05-20 08:54:24 +0200

    added test for RequestOverdrift

--- src/test/java/com/booleanuk/extension/BankTest.java
+++ src/test/java/com/booleanuk/extension/BankTest.java
@@ -5,10 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class BankTest {
     @Test
-    public void testOverdraftApproval(){
+    public void testRequestOverdraft() {
         Bank bank = new Bank();
-        bank.getAccounts().add(new CurrentAccount(0,new Branch("xxx","xx")));
-        bank.getAccounts().add(new SavingAccount(0,new Branch("xxx","xx")));
+        bank.getAccounts().add(new CurrentAccount(0, new Branch("xxx", "xx")));
+        bank.getAccounts().add(new SavingAccount(0, new Branch("xxx", "xx")));
+
+        bank.getoverdraftRequests().add(bank.getAccounts().get(0));
+        bank.getoverdraftRequests().add(bank.getAccounts().get(1));
+
+        Assertions.assertEquals(2, bank.getoverdraftRequests().size());
+    }
+
+    @Test
+    public void testOverdraftApproval() {
+        Bank bank = new Bank();
+        bank.getAccounts().add(new CurrentAccount(0, new Branch("xxx", "xx")));
+        bank.getAccounts().add(new SavingAccount(0, new Branch("xxx", "xx")));
 
         Assertions.assertEquals("Approved", bank.overdraftApproval(bank.getAccounts().get(0)));
         Assertions.assertEquals("Rejected", bank.overdraftApproval(bank.getAccounts().get(1)));

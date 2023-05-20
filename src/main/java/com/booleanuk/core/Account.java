2adb3aa1ad61dee94040b0250dfb706a3f203cac
Dennis <dennisvoutos@gmail.com>
2023-05-20 12:44:48 +0300

    Made the statements look nicer, in the future will use SB

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,15 +1,25 @@
 package com.booleanuk.core;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class Account {
     private Balance balance;
     private String statements;
     public Account(Balance balance){
         setBalance(balance);
-        this.statements = "";
+        this.statements+="\nAccount created at "+convertEpochTimeToDateTime(System.currentTimeMillis())+" with $"+balance+" \n";
+        this.statements += "date       || credit  || debit  || balance\n";
     }
     public Account(int intPart, int decimalPart){
         setBalance(new Balance(intPart,decimalPart));
-        this.statements = "";
+        if(this.balance!= null){
+            this.statements+="Account created at "+convertEpochTimeToDateTime(System.currentTimeMillis())+" with $"+new Balance(intPart,decimalPart)+" \n";
+        }
+        this.statements = "date       || credit  || debit  || balance\n";
+
     }
     //setBalance is private because I don't want any other class to use it.
     private void setBalance(Balance balance) {
@@ -44,11 +54,21 @@ public class Account {
         return false;
     }
     public void addToStatements(Balance balance){
-        this.statements+= "Transaction: "+balance.toString()+"\n";
+        this.statements+= convertEpochTimeToDateTime(System.currentTimeMillis())+ " ||";
+        if(balance.getIntPart()>=0){
+            this.statements+= " $"+balance+" ||          || $"+this.getBalance().toString();
+        }else{
+            this.statements+="         || $"+balance+" || $"+this.getBalance().toString();
+        }
     }
     public String showStatements(){
         System.out.println(statements);
         return this.statements;
     }
-
+    public static String convertEpochTimeToDateTime(long epochTimeInMillis) {
+        Instant instant = Instant.ofEpochMilli(epochTimeInMillis);
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return dateTime.format(formatter);
+    }
 }

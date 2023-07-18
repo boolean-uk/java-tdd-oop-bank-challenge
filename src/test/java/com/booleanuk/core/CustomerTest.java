7aab7d7b3947e929a7431edb3234cfa3c3dc258c
Adam Ciesielski <ciesielski.adam2002@gmail.com>
2023-07-18 11:46:31 +0200

    Customer Tests

---
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -0,0 +1,29 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+
+
+    @Test
+    public void shouldCreateCustomerWithfullNameUSER()
+    {
+        NormalAccount normalAccount = new NormalAccount(123123);
+        SavingsAccount savingsAccount = new SavingsAccount(321321);
+        Customer customer = new Customer("USER",normalAccount,savingsAccount);
+        Assertions.assertEquals("USER",customer.getFullName());
+    }
+    @Test
+    public void shouldCreateCustomerWithNewAccountsProvided()
+    {
+        NormalAccount normalAccount = new NormalAccount(123123);
+        SavingsAccount savingsAccount = new SavingsAccount(321321);
+        Customer customer = new Customer("USER",normalAccount,savingsAccount);
+        Assertions.assertEquals("USER",customer.getFullName());
+
+        Assertions.assertEquals(normalAccount,customer.getNormalAccount());
+        Assertions.assertEquals(savingsAccount,customer.getSavingsAccount());
+    }
+
+}

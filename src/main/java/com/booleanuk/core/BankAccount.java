59330d924124c6ce176e5504ce6949e69275fdbf
PawelCagara <pawel.cagara@talan.com>
2023-07-18 11:06:33 +0200

    depositWithPositiveAmountTest() implemented but it fails as method  deposit() if not fully implemented

--- src/main/java/com/booleanuk/core/BankAccount.java
+++ src/main/java/com/booleanuk/core/BankAccount.java
@@ -4,14 +4,20 @@ public class BankAccount {
 
     private double accountBalance;
     public BankAccount(){
-        accountBalance =0.0;
+        accountBalance =0.00;
+
     }
 
     public boolean deposit(double amount) {
+
         return true;
     }
 
     public boolean withdraw(double amount) {
         return true;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
     }
 }
--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -1,4 +1,14 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CurrentAccountTest {
+
+    @Test
+    public void depositWithPositiveAmountTest() {
+        CurrentAccount account = new CurrentAccount();
+        account.deposit(100);
+        Assertions.assertEquals(100.00, account.getAccountBalance());
+    }
 }

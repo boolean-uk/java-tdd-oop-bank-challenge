b435858119599c5f32649c2d423bb67e4f561aad
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-18 12:41:04 +0200

    Implement the missing generateStatement logic

--- src/main/java/com/booleanuk/core/account/Account.java
+++ src/main/java/com/booleanuk/core/account/Account.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -28,6 +29,32 @@ public abstract class Account {
     }
 
     String generateStatement() {
-        return "";
+        var transactions = new ArrayList<>(this.transactions);
+        Collections.reverse(transactions);
+        var statementBalance = balance;
+        var sb = new StringBuilder();
+        sb.append(String.format(
+                "|| %10s || %10s || %10s || %10s ||\n",
+                "Date", "Credit", "Debit", "Balance"
+        ));
+        for (var t : transactions) {
+            sb
+                    .append(t)
+                    .append(String.format(" %10.2f ||", statementBalance.doubleValue()))
+                    .append("\n");
+            var modifier = BigDecimal.valueOf(t.type().getModifier());
+            statementBalance = statementBalance.subtract(t.amount().multiply(modifier));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        var a = new SavingsAccount();
+        a.deposit(BigDecimal.valueOf(2137));
+        a.withdraw(BigDecimal.valueOf(420));
+        a.deposit(BigDecimal.valueOf(69));
+
+        System.out.println(a.generateStatement());
     }
 }
--- src/main/java/com/booleanuk/core/account/Transaction.java
+++ src/main/java/com/booleanuk/core/account/Transaction.java
@@ -1,20 +1,30 @@
 package com.booleanuk.core.account;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record Transaction(
-    Type type,
-    BigDecimal amount,
-    ZonedDateTime timestamp
+        Type type,
+        BigDecimal amount,
+        ZonedDateTime timestamp
 ) {
     public static Transaction of(Type type, BigDecimal amount) {
         return new Transaction(type, amount, ZonedDateTime.now());
+    }
+
+    @Override
+    public String toString() {
+        var dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return String.format(
+                "|| %-10s || %10.2f || %10.2f ||",
+                timestamp.format(dateFormat),
+                type.equals(Type.Deposit) ? amount.doubleValue() : 0,
+                type.equals(Type.Withdrawal) ? amount.doubleValue() : 0
+        );
     }
 
     @RequiredArgsConstructor

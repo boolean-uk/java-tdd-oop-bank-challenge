a3917a713061461ebad5067c4036ea574c6ea934
Marcin Jankowski <marcin.jankowski@talan.com>
2023-07-18 11:47:55 +0200

    add withdraw method implementation

--- src/main/java/com/booleanuk/core/core/Account.java
+++ src/main/java/com/booleanuk/core/core/Account.java
@@ -16,6 +16,18 @@ public class Account {
         transactions.add(new Transaction(amount));
     }
 
+    public void withdraw(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount must be a positive number");
+        }
+
+        if (getBalance() < amount) {
+            throw new IllegalStateException("Amount must not exceed the balance");
+        }
+
+        transactions.add(new Transaction(amount * -1));
+    }
+
     public double getBalance() {
         BigDecimal sum = transactions.stream()
                 .map(transaction -> BigDecimal.valueOf(transaction.amount))

1dc34100ea398684e746f0db6e7c3fe0f045a150
Julia Girejko <julia.girejko@talan.com>
2023-07-18 12:04:50 +0200

    green stage ClientTest

--- src/test/java/com/booleanuk/core/ClientTest.java
+++ src/test/java/com/booleanuk/core/ClientTest.java
@@ -1,4 +1,13 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ClientTest {
+
+    @Test
+    public void toStringTest() {
+        Client client = new Client("John", "Smith", "01.02.1998", "1234");
+        Assertions.assertEquals("Client{name='John', lastName='Smith', birthDate='01.02.1998', id=1234}", client.toString());
+    }
 }

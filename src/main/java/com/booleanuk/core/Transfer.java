96609a6c158b307cc8129367bbac9c9641b086c4
Kamil Kulak <kamil.kulak@talan.com>
2023-07-18 12:40:42 +0200

    Green stage for transfer tests

--- src/main/java/com/booleanuk/core/Transfer.java
+++ src/main/java/com/booleanuk/core/Transfer.java
@@ -1,4 +1,30 @@
 package com.booleanuk.core;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Transfer {
+    private int amountOfMoney;
+    private LocalDate date;
+
+    public Transfer(int amountOfMoney, LocalDate date) {
+        this.amountOfMoney = amountOfMoney;
+        this.date = date;
+    }
+
+    public int getAmountOfMoney() {
+        return amountOfMoney;
+    }
+
+    public void setAmountOfMoney(int amountOfMoney) {
+        this.amountOfMoney = amountOfMoney;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
--- src/test/java/com/booleanuk/core/TransferTest.java
+++ src/test/java/com/booleanuk/core/TransferTest.java
@@ -11,13 +11,15 @@ public class TransferTest {
     private Transfer transfer;
     @BeforeEach
     public void setup() {
-        transfer = new Transfer(30000, LocalDate.parse("2023-18-07"));
+        transfer = new Transfer(30000, LocalDate.parse("2023-07-18"));
     }
 
     @Test
     public void transferGetterAndSetterShouldReturnProperValue() {
         Assertions.assertEquals(30000, transfer.getAmountOfMoney());
-//        Assertions.assertEquals(2023, transfer.getDate().);
-        
+        Assertions.assertEquals(2023, transfer.getDate().getYear());
+        Assertions.assertEquals(7, transfer.getDate().getMonth().getValue());
+        Assertions.assertEquals(18, transfer.getDate().getDayOfMonth());
+
     }
 }

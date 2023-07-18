a6436b2dcaa82a71a61555a3d211cb3f34619aec
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-18 10:43:51 +0200

    Add UserTest - red

---
+++ src/test/java/com/booleanuk/core/UserTest.java
@@ -0,0 +1,51 @@
+package com.booleanuk.core;
+
+import com.booleanuk.core.banking.BankAccountAlreadyExistsException;
+import com.booleanuk.core.user.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    public void shouldCreateCustomerWithSpecifiedData() {
+        Customer customer = new Customer();
+
+        Assertions.assertNull(customer.getCurrentAccount());
+        Assertions.assertNull(customer.getSavingAccount());
+    }
+
+    @Test
+    public void shouldOpenCurrentAccount() {
+        Customer customer = new Customer();
+        Boolean result = customer.openCurrentAccount();
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldThrowAccountAlreadyExistsExceptionWhenTryingToRecreateCurrentAccount() {
+        Customer customer = new Customer();
+        customer.openCurrentAccount();
+
+        BankAccountAlreadyExistsException bankAccountAlreadyExistsException = Assertions.assertThrows(BankAccountAlreadyExistsException.class, customer::openCurrentAccount);
+        Assertions.assertEquals("You have opened Current Account with ID: " + customer.getCurrentAccount().getUuid(), bankAccountAlreadyExistsException.getMessage());
+    }
+
+    @Test
+    public void shouldOpenSavingAccount() {
+        Customer customer = new Customer();
+        Boolean result = customer.openSavingAccount();
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldThrowAccountAlreadyExistsExceptionWhenTryingToRecreateSavingAccount() {
+        Customer customer = new Customer();
+        customer.openSavingAccount();
+
+        BankAccountAlreadyExistsException bankAccountAlreadyExistsException = Assertions.assertThrows(BankAccountAlreadyExistsException.class, customer::openSavingAccount);
+        Assertions.assertEquals("You have opened Saving Account with ID: " + customer.getSavingAccount().getUuid(), bankAccountAlreadyExistsException.getMessage());
+    }
+}

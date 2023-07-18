094a9586e15ce4f7ab8d94b1222750554bc34ee2
Agnieszka Zagorna <a.h.zagorna@gmail.com>
2023-07-18 23:40:30 +0200

    added user interface

--- src/main/java/com/booleanuk/core/BankAccount.java
+++ src/main/java/com/booleanuk/core/BankAccount.java
@@ -1,6 +1,7 @@
 package com.booleanuk.core;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.TreeMap;
 
 public abstract class BankAccount {
@@ -8,7 +9,30 @@ public abstract class BankAccount {
     private TreeMap<LocalDateTime, Integer> transactions = new TreeMap<>();
 
     public String generateBankStatement(){
-        return "";
+        int balance = 0;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String bankStatement = "date       || credit  || debit  || balance \n";
+        for(LocalDateTime date : transactions.navigableKeySet()) {
+            bankStatement += date.format(formatter) + " || ";
+            int amount = transactions.get(date);
+            if(amount >= 0){
+                balance += amount;
+                bankStatement += (double) amount/100;
+                if (amount>=1000) bankStatement += "  ";
+                else if (amount>=100) bankStatement += "       ";
+                else bankStatement += "        ";
+                bankStatement+= "||        || ";
+                bankStatement += (double) balance/100 + "\n";
+            } else {
+                balance += amount;
+                bankStatement += "        || ";
+                bankStatement += (double) -(amount/100);
+                if (amount<=-1000) bankStatement += "  || ";
+                else if (amount<=-100) bankStatement += "    || ";
+                else bankStatement += "       || ";
+                bankStatement += (double) balance/100 + "\n";
+            }
+        } return bankStatement;
     }
 
     public void depositMoney(double amount){
@@ -28,7 +52,8 @@ public abstract class BankAccount {
         for(LocalDateTime date : transactions.keySet()){
             balance = balance + transactions.get(date);
         }
-        return (double) balance/100;
+        balance = balance >= 0 ? balance : -balance;
+        return (balance/100);
     }
 
     public void requestOverdraft(double amount){}

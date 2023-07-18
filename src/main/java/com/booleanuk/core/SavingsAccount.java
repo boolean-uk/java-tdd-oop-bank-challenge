ab784a4e374557cfa909a45b58180637d0277dcf
Kamil Kulak <kamil.kulak@talan.com>
2023-07-18 11:57:09 +0200

    Green stage for savingsAccount tests

--- src/main/java/com/booleanuk/core/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/SavingsAccount.java
@@ -1,4 +1,8 @@
 package com.booleanuk.core;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account{
+
+    public SavingsAccount(String accountNr, Branch branch) {
+        super(accountNr, branch);
+    }
 }

c7d64b4ecdc1615e08cbb140f2dfaedeb4d1241f
Marlena Luczak <dziubichmarlena@gmail.com>
2023-07-18 13:07:58 +0200

    method for adding accounts

--- src/main/java/com/booleanuk/core/Customer.java
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -13,4 +13,18 @@ public class Customer {
         this.accounts = new ArrayList<>();
     }
 
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void createAccount(String typeOfAccount){
+        if(typeOfAccount.equalsIgnoreCase("saving")){
+            SavingAccount savingAccount = new SavingAccount(typeOfAccount);
+            accounts.add(savingAccount);
+        } else if(typeOfAccount.equalsIgnoreCase("current")){
+            CurrentAccount currentAccount = new CurrentAccount(typeOfAccount);
+            accounts.add(currentAccount);
+        }
+        throw new RuntimeException("We don't have this type of account, sorry!");
+    }
 }

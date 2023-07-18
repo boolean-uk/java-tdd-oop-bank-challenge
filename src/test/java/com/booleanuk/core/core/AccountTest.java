9168d04590c18750b26385670378ecbb3e052d41
Marcin Jankowski <marcin.jankowski@talan.com>
2023-07-18 12:20:30 +0200

    add test for generateStatement

--- src/test/java/com/booleanuk/core/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/core/AccountTest.java
@@ -3,6 +3,9 @@ package com.booleanuk.core.core;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -45,5 +48,21 @@ class AccountTest {
     @Test
     void withdrawThrowsExceptionForNegativeAmount() {
         assertThrows(IllegalArgumentException.class, () -> account.withdraw(-20));
+    }
+
+    @Test
+    void generateStatementReturnsStatement() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String date = LocalDateTime.now().format(formatter);
+        account.deposit(19.99);
+        account.deposit(19.99);
+        account.withdraw(19.99);
+
+        assertEquals("""
+                date || credit || debit  || balance
+                %s   ||        || 19.99  || 19.99
+                %s   || 19.99  ||        || 39.98
+                %s   || 19.99  ||        || 19.99
+                """.formatted(date, date, date), account.generateStatement());
     }
 }

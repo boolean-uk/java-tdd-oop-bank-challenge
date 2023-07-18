bdbd26037bde3667ecbe2ade21e9c17f12954d3d
Jan Rusak <jrusak001@gmail.com>
2023-07-18 14:49:39 +0200

    Add BankAccount in core

--- src/main/java/com/booleanuk/core/BankAccount.java
+++ src/main/java/com/booleanuk/core/BankAccount.java
@@ -1,4 +1,43 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankAccount {
+    protected final List<Transaction> transactions;
+
+    public BankAccount() {
+        this.transactions = new ArrayList<>();
+    }
+
+    public boolean deposit(int amount) {
+        if (amount <= 0)
+            return false;
+
+        transactions.add(new Transaction(amount, true));
+        return true;
+    }
+
+    public boolean withdraw(int amount) {
+        if (amount <= 0 || getCurrentBalance() - amount < 0)
+            return false;
+
+        transactions.add(new Transaction(amount, false));
+        return true;
+    }
+
+    protected int getCurrentBalance() {
+        return transactions.stream()
+                .map(t -> t.isDeposit() ? t.getAmount() : -t.getAmount())
+                .reduce(Integer::sum)
+                .orElse(0);
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void clearAccount() {
+        transactions.clear();
+    }
 }

7e0cffac94fc3be66ec5196c018583f935052c58
Wojciech Żółkowski <wojciechzolkowski@gmail.com>
2023-07-18 15:03:15 +0200

    Fixed wire function

--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -12,7 +12,12 @@ public class CurrentAccount extends Account{
     }
 
     public void wire(double amount, String accountNumber) {
-        if(this.getBalance() >= amount) this.getTransactions().add(new Transaction(WIRE_TO,amount));
+        if(this.getBalance() >= amount){
+            this.getTransactions().add(new Transaction(WIRE_TO,amount));
+            Account receiver = Bank.getAccount(accountNumber);
+            receiver.getTransactions().add(new Transaction(WIRE_TO,amount));
+
+        }
         else throw new IllegalArgumentException("You don't have enough money");
     }
 }

3b16c02ed9a4107aee26fff241b71fabcc86ce20
Agnieszka Zagorna <a.h.zagorna@gmail.com>
2023-07-18 22:34:07 +0200

    added CurrentAccount method

--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -1,4 +1,10 @@
 package com.booleanuk.core;
 
 public class CurrentAccount extends BankAccount{
+    private int overdraft = 0;
+
+    @Override
+    public void requestOverdraft(double amount){
+        overdraft = (int) amount*100;
+    }
 }

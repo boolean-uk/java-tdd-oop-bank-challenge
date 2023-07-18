ad0731585a41728d89ae59ab5bbc44382f38431f
PawelCagara <pawel.cagara@talan.com>
2023-07-18 10:58:05 +0200

    Test for adding CurrentAccount and SavingAccount to bank

--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -3,7 +3,7 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CurrentAccount {
+public class CurrentAccount extends BankAccount{
 
     private List<Transaction> transactions;
 

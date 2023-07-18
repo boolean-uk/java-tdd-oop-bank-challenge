f9a87215080ed7d2331e4af670eebece11087a27
Martyna <martyna.marszalek@talan.com>
2023-07-18 12:55:43 +0200

    Adding test for withdraw method

--- src/test/java/com/booleanuk/core/CustomerTest.java
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -24,4 +24,16 @@ public class CustomerTest {
         Assertions.assertEquals(1, customer.getAccounts().size());
         Assertions.assertEquals("savings", customer.getAccounts().get(0).getType());
     }
+    @Test
+    public void shouldWithdraw() {
+        int customerId = 123;
+        Customer customer = new Customer(customerId);
+        customer.createCurrentAccount("current", 1000);
+
+        Assertions.assertTrue(customer.withdraw(300, "current"));
+
+        int expectedBalance = 700;
+        int actualBalance = customer.getAccounts().get(0).getBalance();
+        Assertions.assertEquals(expectedBalance, actualBalance);
+    }
 }

f0b89104cf33f0df9a3f377b575773467fd53d3c
Agnieszka Zagorna <a.h.zagorna@gmail.com>
2023-07-18 21:54:45 +0200

    added BankBranch tests failing

--- src/main/java/com/booleanuk/core/BankBranch.java
+++ src/main/java/com/booleanuk/core/BankBranch.java
@@ -1,5 +1,27 @@
 package com.booleanuk.core;
 
 public class BankBranch {
+    public int createAccount(boolean isCurrentAccount){
+        return 0;
+    }
 
+    public String getBankStatement(int accountNumber){
+        return "";
+    }
+
+    public void depositMoney(int accountNumber, double amount){
+
+    }
+
+    public void withdrawMoney(int accountNumber, double amount){
+
+    }
+
+    public void requestOverdraft(int accountNumber, double amount) {
+
+    }
+
+    public BankAccount getAccount(int accountNumber){
+        return new SavingsAccount();
+    }
 }

1c0887b74798b8f6f20ada1c945f3ae40c954046
Pawel Kwiecinski <pawel.kwiecinski@talan.com>
2023-07-18 11:06:37 +0200

    green stage of makeTransactionTests

--- src/main/java/com/booleanuk/core/BankAccount.java
+++ src/main/java/com/booleanuk/core/BankAccount.java
@@ -1,2 +1,22 @@
-package com.booleanuk.core;public class BankAccount {
+package com.booleanuk.core;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class BankAccount {
+    LinkedHashMap<String, List<Transaction>> accounts;
+
+    public BankAccount() {
+        accounts = new LinkedHashMap<>();
+        accounts.put("main", new ArrayList<>());
+    }
+
+    public boolean makeTransaction(String accountName, Transaction transaction) {
+        if(!accounts.containsKey(accountName))
+            return false;
+
+        accounts.get(accountName).add(transaction);
+        return true;
+    }
 }
--- src/test/java/com/booleanuk/core/BankAccountTest.java
+++ src/test/java/com/booleanuk/core/BankAccountTest.java
@@ -1,2 +1,49 @@
-package com.booleanuk.core;public class BankAccountTest {
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class BankAccountTest {
+
+    @Test
+    public void constructorTest() {
+        new BankAccount();
+    }
+
+    @Test
+    public void makeTransactionTest_WhenEverythingIsCorrect_ShouldReturnTrue () {
+        //Given
+        BankAccount bankAccount = new BankAccount();
+
+        LocalDate date = LocalDate.parse("2023-07-20", DateTimeFormatter.ISO_LOCAL_DATE);
+        Transaction transaction1 = new Transaction(date, 1000);
+        Transaction transaction2 = new Transaction(date, -500);
+
+        //When
+        boolean result1 = bankAccount.makeTransaction("main", transaction1);
+        boolean result2 = bankAccount.makeTransaction("main", transaction2);
+
+        //Then
+        Assertions.assertTrue(result1);
+        Assertions.assertTrue(result2);
+    }
+
+    @Test
+    public void makeTransactionTest_WhenAccountNameIsInvalid_ShouldReturnFalse () {
+        //Given
+        BankAccount bankAccount = new BankAccount();
+
+        LocalDate date = LocalDate.parse("2023-07-20", DateTimeFormatter.ISO_LOCAL_DATE);
+        Transaction transaction1 = new Transaction(date, 1000);
+
+        //When
+        boolean result1 = bankAccount.makeTransaction("mainn", transaction1);
+
+        //Then
+        Assertions.assertFalse(result1);
+    }
 }

6ddb0e36b5fcc1f774f4ce5c55447c5eed172dd6
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-18 12:23:41 +0200

    another refactoring of AccountTest class

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -14,53 +14,74 @@ public class AccountTest {
     @Order(1)
     @Test
     public void testDeposit_ShouldReturnTrueAndBalanceShouldBeCorrect(){
+        //Given
+        float depositAmount1 = 1000F;
+        float depositAmount2 = 1500F;
+        float expectedResult = depositAmount1 + depositAmount2;
+
+
         //When
-        account.deposit(1000F);
-        boolean depositResult = account.deposit(1500F);
+        account.deposit(depositAmount1);
+        boolean depositResult = account.deposit(depositAmount2);
         float checkBalanceResult = account.checkBalance();
 
         //Then
         Assertions.assertTrue(depositResult);
-        Assertions.assertEquals(2500, checkBalanceResult);
+        Assertions.assertEquals(expectedResult, checkBalanceResult);
     }
 
     @Order(2)
     @Test
     public void testDeposit_WhenAmountNotValid_ShouldReturnFalseAndBalanceShouldNotChange(){
+        //Given
+        float depositAmount = 1000F;
+        float invalidDepositAmount = -1500F;
+
         //When
-        account.deposit(1000F);
-        boolean depositResult = account.deposit(-1500F);
+        account.deposit(depositAmount);
+        boolean depositResult = account.deposit(invalidDepositAmount);
         float checkBalanceResult = account.checkBalance();
 
         //Then
         Assertions.assertFalse(depositResult);
-        Assertions.assertEquals(1000, checkBalanceResult);
+        Assertions.assertEquals(depositAmount, checkBalanceResult);
     }
 
     @Order(3)
     @Test
     public void testWithdraw_ShouldReturnTrueAndBalanceShouldBeCorrect(){
+        //Given
+        float depositAmount = 1000F;
+        float withdrawAmount1 = 300F;
+        float withdrawAmount2 = 300F;
+        float expectedResult = depositAmount - withdrawAmount1 - withdrawAmount2;
+
         //When
-        account.deposit(1000F);
-        account.withdraw(300F);
-        boolean withdrawResult = account.withdraw(400F);
+        account.deposit(depositAmount);
+        account.withdraw(withdrawAmount1);
+        boolean withdrawResult = account.withdraw(withdrawAmount2);
         float checkBalanceResult = account.checkBalance();
 
         //Then
         Assertions.assertTrue(withdrawResult);
-        Assertions.assertEquals(300, checkBalanceResult);
+        Assertions.assertEquals(expectedResult, checkBalanceResult);
     }
 
     @Order(4)
     @Test
     public void testWithdraw_WhenAmountNotValid_ShouldReturnFalseAndBalanceShouldNotChange(){
+        //Given
+        float depositAmount = 1000F;
+        float invalidWithdrawAmount = -400F;
+
+
         //When
-        account.deposit(1000F);
-        boolean withdrawResult = account.withdraw(-400F);
+        account.deposit(depositAmount);
+        boolean withdrawResult = account.withdraw(invalidWithdrawAmount);
         float checkBalanceResult = account.checkBalance();
 
         //Then
         Assertions.assertFalse(withdrawResult);
-        Assertions.assertEquals(1000, checkBalanceResult);
+        Assertions.assertEquals(depositAmount, checkBalanceResult);
     }
 }

569eb3bb1958e71b6eb72cc36c1de2ce505ed67b
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-18 11:57:07 +0200

    red stage of testDeposit_WhenAmountNotValid_ShouldReturnFalseAndBalanceShouldNotChange

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -18,4 +18,19 @@ public class AccountTest {
         Assertions.assertTrue(depositResult);
         Assertions.assertEquals(2500, checkBalanceResult);
     }
+
+    @Test
+    public void testDeposit_WhenAmountNotValid_ShouldReturnFalseAndBalanceShouldNotChange(){
+        //Given
+        Account account = new Account();
+
+        //When
+        account.deposit(1000F);
+        boolean depositResult = account.deposit(-1500F);
+        float checkBalanceResult = account.checkBalance();
+
+        //Then
+        Assertions.assertTrue(depositResult);
+        Assertions.assertEquals(1000, checkBalanceResult);
+    }
 }

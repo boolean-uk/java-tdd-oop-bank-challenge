6388c08bc313ec232f0f048e5d48af7e1b1da222
Kornel <korneldabrowski@gmail.com>
2023-07-18 11:08:25 +0200

    test for withdraw

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -39,4 +39,17 @@ public class AccountTest {
         Assertions.assertEquals(4000, myAccount.getBalance());
     }
 
+    @Test
+    public void testWithdraw() {
+
+        myAccount.setBalance(1000);
+        myAccount.deposit(1000);
+        myAccount.deposit(1000);
+        myAccount.deposit(1000);
+
+        myAccount.withdraw(4000);
+
+        Assertions.assertEquals(0, myAccount.getBalance());
+    }
+
 }

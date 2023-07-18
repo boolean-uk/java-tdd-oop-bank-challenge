7c9e79ecfaa2b766847152f780f3be04006fb88f
Martyna <martyna.marszalek@talan.com>
2023-07-18 11:58:55 +0200

    Creating test isBankCreated()

--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -1,4 +1,14 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BankTest {
+
+    @Test
+    public void isBankCreated(){
+        Bank bank = new Bank();
+        Assertions.assertTrue(bank.getCustomers().isEmpty());
+        Assertions.assertTrue(bank.getAccounts().isEmpty());
+    }
 }

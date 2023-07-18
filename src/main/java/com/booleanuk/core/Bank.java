1d50b05d1ef82a4eef5e460eb3c6775f976111d0
Michal Siek <michal.siek21@gmail.com>
2023-07-18 18:21:41 +0200

    core + tests completed

--- src/main/java/com/booleanuk/core/Bank.java
+++ src/main/java/com/booleanuk/core/Bank.java
@@ -1,4 +1,33 @@
 package com.booleanuk.core;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+
 public class Bank {
+    CustomerAccount customerAccount;
+    StringBuilder bankID;
+    HashMap<String, CustomerAccount> accounts = new HashMap<String, CustomerAccount>();
+
+
+
+    public Bank(String bankID) {
+        this.bankID = new StringBuilder(bankID);
+    }
+
+    public void addAccount(String name, CustomerAccount customerAccount) {
+        accounts.put(name,customerAccount);
+    }
+
+    public static void main(String[] args) {
+        Bank bank = new Bank("Test Bank");
+        bank.addAccount("Michał Siek", new CustomerAccount(AccountType.CURRENT));
+
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(100),true);
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(400),true);
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(200),false);
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(500),false);
+
+        System.out.println(bank.accounts.get("Michał Siek").printStatement());
+
+    }
 }
--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 
 public class BankTest {
 
@@ -12,47 +11,50 @@ public class BankTest {
 
     @Test
     public void shouldCreateCurrentAccount(){
-        Bank bank = new Bank();
-        Assertions.assertDoesNotThrow(() -> bank.addAccount());
+        Bank bank = new Bank("Test Bank");
+        bank.addAccount("Michał Siek", new CustomerAccount(AccountType.CURRENT));
+        Assertions.assertDoesNotThrow(() -> bank.accounts.get("Michał Siek"));
 
     }
-
     @Test
-    public void shouldGenerateBankStatement(){
-        Bank bank = new Bank();
-        Customer customer = new Customer();
+    public void shouldCreateSavingsAccount(){
+        Bank bank = new Bank("Test Bank");
+        bank.addAccount("Michał Siek",new CustomerAccount(AccountType.SAVINGS));
+        Assertions.assertDoesNotThrow(() -> bank.accounts.get("Michał Siek"));
 
-        ArrayList<String> test_statement = new ArrayList<>();
-        test_statement.add("date       || credit  || debit  || balance");
-        test_statement.add("14/01/2012 ||         || 500.00 || 2500.00");
-        Assertions.assertEquals(test_statement,customer.printStatement());
     }
+
 
     @Test
     public void shouldDepositMoney(){
-        Customer customer = new Customer();
-        customer.deposit(BigDecimal.valueOf(1000));
-        Assertions.assertEquals(REF_ACCOUNT_BALANCE, customer.getBalance());
+        Bank bank = new Bank("Test Bank");
+        bank.addAccount("Michał Siek",new CustomerAccount(AccountType.SAVINGS));
+
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(200),true);
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(500),true);
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(100),true);
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(200),true);
+
+        Assertions.assertEquals(REF_ACCOUNT_BALANCE, bank.accounts.get("Michał Siek").totalBalance());
     }
 
     @Test
     public void shouldWithdrawMoney(){
-        Customer customer = new Customer();
-        customer.withdraw(BigDecimal.valueOf(1000));
-        Assertions.assertEquals(BigDecimal.ZERO, customer.getBalance());
+        Bank bank = new Bank("Test Bank");
+        bank.addAccount("Michał Siek",new CustomerAccount(AccountType.CURRENT));
+
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(1200),true);
+        Assertions.assertEquals(BigDecimal.valueOf(1200), bank.accounts.get("Michał Siek").totalBalance());
+
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(200),false);
+        Assertions.assertEquals(REF_ACCOUNT_BALANCE, bank.accounts.get("Michał Siek").totalBalance());
     }
 
     @Test
     public void shouldWithdrawMoneyIntoDebt(){
-        Customer customer = new Customer();
-        customer.withdraw(BigDecimal.valueOf(2000));
-        Assertions.assertEquals(REF_ACCOUNT_BALANCE.multiply(BigDecimal.valueOf(-1)), customer.getBalance());
+        Bank bank = new Bank("Test Bank");
+        bank.addAccount("Michał Siek",new CustomerAccount(AccountType.CURRENT));
+        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(1000),false);
+        Assertions.assertEquals(REF_ACCOUNT_BALANCE.multiply(BigDecimal.valueOf(-1)), bank.accounts.get("Michał Siek").totalBalance());
     }
-
-    @Test
-    public void shouldBlockWithdrawingMoneyIntoDebt(){
-        Customer customer = new Customer();
-        Assertions.assertThrows(IllegalStateException.class, () -> customer.withdraw(BigDecimal.valueOf(3000)));
-    }
-
 }

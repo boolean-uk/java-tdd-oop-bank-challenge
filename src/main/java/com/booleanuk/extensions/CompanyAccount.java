1fc18063c3ce9df4d6cb664fb64390a0155cf813
Justyna Dudar <justyna.dudar@talan.com>
2023-07-18 22:51:26 +0200

    Implementation for CompanyAccount associated with specific branches (Green)

--- src/main/java/com/booleanuk/extensions/CompanyAccount.java
+++ src/main/java/com/booleanuk/extensions/CompanyAccount.java
@@ -3,7 +3,7 @@ package com.booleanuk.extensions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CompanyAccount extends Account{
+public class CompanyAccount extends Account {
     List<BranchAccount> branchAccounts;
 
     public CompanyAccount() {
@@ -13,5 +13,23 @@ public class CompanyAccount extends Account{
 
     public List<BranchAccount> getBranchAccounts() {
         return branchAccounts;
+    }
+
+    public String createBranchAccount(String branch) {
+        if (branchAccounts.stream().anyMatch(e -> e.getBranch().equals(branch))) {
+            return "A branch account in " + branch + " exists.";
+        } else {
+            BranchAccount branchAccount = new BranchAccount(branch);
+            branchAccounts.add(branchAccount);
+            return "A branch account in " + branch + " was created.";
+        }
+    }
+
+    public float getBalanceFromAllBranches() {
+        float balance = 0;
+        for(BranchAccount branchAccount: branchAccounts){
+            balance += branchAccount.countTheBalance();
+        }
+        return balance;
     }
 }

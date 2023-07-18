7c5e981f875f43ac05a1c0d34281270e3f2c76b1
Kamil Kulak <kamil.kulak@talan.com>
2023-07-18 12:45:32 +0200

    changed transfer test

--- src/test/java/com/booleanuk/core/TransferTest.java
+++ src/test/java/com/booleanuk/core/TransferTest.java
@@ -20,5 +20,12 @@ public class TransferTest {
         Assertions.assertEquals(7, transfer.getDate().getMonth().getValue());
         Assertions.assertEquals(18, transfer.getDate().getDayOfMonth());
 
+        transfer.setAmountOfMoney(40000);
+        transfer.setDate(LocalDate.parse("2023-08-19"));
+
+        Assertions.assertEquals(40000, transfer.getAmountOfMoney());
+        Assertions.assertEquals(2023, transfer.getDate().getYear());
+        Assertions.assertEquals(8, transfer.getDate().getMonth().getValue());
+        Assertions.assertEquals(19, transfer.getDate().getDayOfMonth());
     }
 }

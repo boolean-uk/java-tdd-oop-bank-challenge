5a50a46fb2ce28213b672207d40d9356b1013bf9
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-18 16:00:10 +0200

    Refactor test of creating a bank statement

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -23,14 +23,15 @@ public class AccountTest {
         UUID accountId = bank.openAccount(customerId, CurrentAccount.class);
         Account account = bank.getAccounts().get(accountId);
         List<Transaction> transactions = account.getTransactions();
-        transactions.add(new Transaction(LocalDateTime.of(2012, 1, 14, 0, 0, 0), null, BigDecimal.valueOf(1_000), BigDecimal.valueOf(1_000)));
-        transactions.add(new Transaction(LocalDateTime.of(2023, 1, 13, 0, 0, 0), null, BigDecimal.valueOf(2_000), BigDecimal.valueOf(3_000)));
-        transactions.add(new Transaction(LocalDateTime.of(2023, 1, 10, 0, 0, 0), BigDecimal.valueOf(500), null, BigDecimal.valueOf(2_500)));
+        transactions.add(new Transaction(LocalDateTime.of(2012, 1, 14, 0, 0, 0), BigDecimal.valueOf(1_000), null, BigDecimal.valueOf(1_000)));
+        transactions.add(new Transaction(LocalDateTime.of(2012, 1, 13, 0, 0, 0), BigDecimal.valueOf(2_000), null, BigDecimal.valueOf(3_000)));
+        transactions.add(new Transaction(LocalDateTime.of(2012, 1, 10, 0, 0, 0), null, BigDecimal.valueOf(500), BigDecimal.valueOf(2_500)));
         String statement = account.generateStatement();
+
         Assertions.assertTrue(statement.contains("      date ||     credit ||      debit ||    balance"));
-        Assertions.assertTrue(statement.contains("13/01/2023 ||            ||    2000.00 ||    3000.00"));
-        Assertions.assertTrue(statement.contains("10/01/2023 ||     500.00 ||            ||    2500.00"));
-        Assertions.assertTrue(statement.contains("14/01/2012 ||            ||    1000.00 ||    1000.00"));
+        Assertions.assertTrue(statement.contains("14/01/2012 ||    1000.00 ||            ||    1000.00"));
+        Assertions.assertTrue(statement.contains("13/01/2012 ||    2000.00 ||            ||    3000.00"));
+        Assertions.assertTrue(statement.contains("10/01/2012 ||            ||     500.00 ||    2500.00"));
     }
 
     @Test

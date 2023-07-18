5ad2e112896c279186288c8012ede17af33fd834
Marlena Luczak <dziubichmarlena@gmail.com>
2023-07-18 18:31:50 +0200

    classes added to extension, methods refactored

--- src/main/java/com/booleanuk/extension/BankManager.java
+++ src/main/java/com/booleanuk/extension/BankManager.java
@@ -8,4 +8,18 @@ public class BankManager {
         this.fullName = fullName;
     }
 
+    // only current accounts can have overdraft
+    public boolean changeStatusOfOverDraftRequest(Customer customer, String accountNumber) {
+        Account currentAccount = customer.getAllAccounts().stream()
+                .filter(account -> account.getTypeOfAccount().equals("current"))
+                .filter(account -> account.getAccountNumber().equals(accountNumber))
+                .findFirst()
+                .get();
+
+        if (currentAccount instanceof CurrentAccount) {
+            ((CurrentAccount) currentAccount).setOverdraft(true);
+            return true;
+        }
+        return false;
+    }
 }

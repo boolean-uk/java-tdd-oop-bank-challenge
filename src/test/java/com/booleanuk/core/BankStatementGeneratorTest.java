0763ceffe8a962631225b4237c7fb673a3dbb9cb
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-07-18 16:40:40 +0200

    Extensions 1-4 done

--- src/test/java/com/booleanuk/core/BankStatementGeneratorTest.java
+++ src/test/java/com/booleanuk/core/BankStatementGeneratorTest.java
@@ -13,14 +13,14 @@ public class BankStatementGeneratorTest {
         Account account = new Account("Jan Lisek","00003");
         account.depositMoney(1000);
         account.withdrawMoney(300);
-        account.depositMoney(4000);
+        account.depositMoney(5000);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String date = LocalDate.now().format(formatter);
         String expectedStatement = "date       || credit  || debit  || balance \n" +
                 date + " || 1000,00 ||        || 1000,00\n" +
                 date + " ||         || 300,00 ||  700,00\n" +
-                date + " || 4000,00 ||        || 4700,00\n";
+                date + " || 5000,00 ||        || 5700,00\n";
 
         Assertions.assertEquals(expectedStatement, account.generateBankStatement());
     }

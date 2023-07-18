ea1c1e3fa6a1edcd56c79dfc8907b32f3aa5fa82
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-18 11:53:33 +0200

    red stage of testDeposit_ShouldReturnTrueAndBalanceShouldBeCorrect

---
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -0,0 +1,21 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    @Test
+    public void testDeposit_ShouldReturnTrueAndBalanceShouldBeCorrect(){
+        //Given
+        Account account = new Account();
+
+        //When
+        account.deposit(1000F);
+        boolean depositResult = account.deposit(1500F);
+        float checkBalanceResult = account.checkBalance();
+
+        //Then
+        Assertions.assertTrue(depositResult);
+        Assertions.assertEquals(2500, checkBalanceResult);
+    }
+}

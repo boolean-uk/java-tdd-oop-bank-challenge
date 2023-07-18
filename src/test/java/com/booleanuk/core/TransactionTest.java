cb8609f6079aba1ab24d46bd80a4d96329ab4765
Jan Rusak <jrusak001@gmail.com>
2023-07-18 14:29:50 +0200

    Add TransactionTest

--- src/test/java/com/booleanuk/core/TransactionTest.java
+++ src/test/java/com/booleanuk/core/TransactionTest.java
@@ -1,4 +1,36 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TransactionTest {
+
+    @Test
+    public void createTransaction() {
+        Transaction transaction = new Transaction(
+                1000,
+                true
+        );
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern(
+                        "uuuu/MM/dd"
+                );
+
+        assertEquals(
+                dateTime.format(formatter),
+                transaction.getDate().format(formatter)
+        );
+        assertEquals(
+                1000,
+                transaction.getAmount()
+        );
+        assertTrue(transaction.isDeposit());
+    }
 }

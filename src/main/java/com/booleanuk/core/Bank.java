f951dd61ed736b10455480bedcedf9b5e5917cdc
Michal Siek <michal.siek21@gmail.com>
2023-07-18 18:35:41 +0200

    added domain model

--- src/main/java/com/booleanuk/core/Bank.java
+++ src/main/java/com/booleanuk/core/Bank.java
@@ -17,17 +17,4 @@ public class Bank {
     public void addAccount(String name, CustomerAccount customerAccount) {
         accounts.put(name,customerAccount);
     }
-
-    public static void main(String[] args) {
-        Bank bank = new Bank("Test Bank");
-        bank.addAccount("Michał Siek", new CustomerAccount(AccountType.CURRENT));
-
-        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(100),true);
-        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(400),true);
-        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(200),false);
-        bank.accounts.get("Michał Siek").transaction(BigDecimal.valueOf(500),false);
-
-        System.out.println(bank.accounts.get("Michał Siek").printStatement());
-
-    }
 }

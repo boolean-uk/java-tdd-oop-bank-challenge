443afd43fe489c2089fe5062725cd3486ab18666
PawelCagara <pawel.cagara@talan.com>
2023-07-18 12:26:00 +0200

    test for deposit to SavingAccount

--- src/main/java/com/booleanuk/core/SavingAccount.java
+++ src/main/java/com/booleanuk/core/SavingAccount.java
@@ -1,9 +1,35 @@
 package com.booleanuk.core;
 
+import java.util.Date;
+import java.util.List;
+
 public class SavingAccount implements BankAccount{
+
+    private List<Transaction> transactions;
+    private double balance;
+
+    public SavingAccount() {
+        this.balance = 0;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
     @Override
     public boolean deposit(double amount) {
-        return false;
+        if(amount > 0) {
+            balance += amount;
+            transactions.add(new Transaction(new Date(), "credit", amount,getBalance()));
+            return true;
+        }else {
+            System.out.println("Amount must be a positive number");
+            return false;
+        }
     }
 
     @Override
--- src/test/java/com/booleanuk/core/SavingAccountTest.java
+++ src/test/java/com/booleanuk/core/SavingAccountTest.java
@@ -1,4 +1,38 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SavingAccountTest {
+
+    CurrentAccount account;
+    public SavingAccountTest() {
+        account = new CurrentAccount();
+    }
+
+    @Test
+    public void depositWithPositiveAmountTest() {
+
+        account.deposit(100);
+
+        Assertions.assertEquals(100.00, account.getBalance());
+        Assertions.assertEquals(1, account.getTransactions().size());
+        account.deposit(100.55);
+        Assertions.assertEquals(200.55, account.getBalance());
+        account.deposit(100.44);
+        Assertions.assertEquals(300.99, account.getBalance());
+        Assertions.assertEquals(3,account.getTransactions().size());
+    }
+
+    @Test
+    public void depositWith0OrNegativeAmountTest() {
+        account.deposit(100.11);
+        Assertions.assertEquals(100.11, account.getBalance());
+        Assertions.assertEquals(1, account.getTransactions().size());
+
+        account.deposit(0);
+        account.deposit(-100.11);
+        Assertions.assertEquals(100.11, account.getBalance());
+        Assertions.assertEquals(1, account.getTransactions().size());
+    }
 }

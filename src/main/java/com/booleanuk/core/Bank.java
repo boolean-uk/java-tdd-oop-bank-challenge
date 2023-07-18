0ef94361787491efabe18ef2ddf8d281d90f53f3
Martyna <martyna.marszalek@talan.com>
2023-07-18 12:01:20 +0200

    Implementation getters for Bank and constructor

--- src/main/java/com/booleanuk/core/Bank.java
+++ src/main/java/com/booleanuk/core/Bank.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class Bank {
     private ArrayList<Account> accounts;
     private ArrayList<Customer> customers;
+
+    public Bank() {
+        this.customers = new ArrayList<>();
+        this.accounts = new ArrayList<>();
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
 }

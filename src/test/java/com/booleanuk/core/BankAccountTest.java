864ef35b08409a07b8da0abbd01b7ed8b05bd1f5
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-18 11:35:59 +0200

    Deposit to SavingAccount - red test

--- src/test/java/com/booleanuk/core/BankAccountTest.java
+++ src/test/java/com/booleanuk/core/BankAccountTest.java
@@ -15,13 +15,13 @@ public class BankAccountTest {
     Customer customer = new Customer();
 
     @Test
-    public void shouldThrowBankAccountNotOpenedWhenTryingToDepositWithoutAccount() {
+    public void shouldThrowBankAccountNotOpenedWhenTryingToDepositWithoutCurrentAccount() {
         BankAccountNotOpened bankAccountNotOpened = Assertions.assertThrows(BankAccountNotOpened.class, () -> customer.depositCurrentAccount(BigDecimal.valueOf(1000)));
         Assertions.assertEquals("Current Account not opened", bankAccountNotOpened.getMessage());
     }
 
     @Test
-    public void shouldSuccessfullyDeposit() {
+    public void shouldSuccessfullyDepositCurrentAccount() {
         customer.openCurrentAccount();
         BigDecimal transactionAmount = BigDecimal.valueOf(1000);
         BankTransaction result = customer.depositCurrentAccount(transactionAmount);
@@ -32,4 +32,24 @@ public class BankAccountTest {
         Assertions.assertEquals(transactionAmount, result.getBalanceAfter());
         Assertions.assertEquals(TransactionType.DEPOSIT, result.getTransactionType());
     }
+
+    @Test
+    public void shouldThrowBankAccountNotOpenedWhenTryingToDepositWithoutSavingAccount() {
+        BankAccountNotOpened bankAccountNotOpened = Assertions.assertThrows(BankAccountNotOpened.class, () -> customer.depositSavingAccount(BigDecimal.valueOf(1000)));
+        Assertions.assertEquals("Saving Account not opened", bankAccountNotOpened.getMessage());
+    }
+
+    @Test
+    public void shouldSuccessfullyDeposit() {
+        customer.openSavingAccount();
+        BigDecimal transactionAmount = BigDecimal.valueOf(2000);
+        BankTransaction result = customer.depositSavingAccount(transactionAmount);
+
+        Assertions.assertEquals(transactionAmount, result.getAmount());
+        Assertions.assertEquals(TransactionResult.SUCCESSFUL, result.getTransactionResult());
+        Assertions.assertEquals(BigDecimal.ZERO, result.getBalanceBefore());
+        Assertions.assertEquals(transactionAmount, result.getBalanceAfter());
+        Assertions.assertEquals(TransactionType.DEPOSIT, result.getTransactionType());
+    }
+
 }

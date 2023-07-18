0d631c3afca9a2258b246e23d5132840c07ec305
Jan Rusak <jrusak001@gmail.com>
2023-07-18 16:30:27 +0200

    Add BankTest in extension

--- src/test/java/com/booleanuk/extension/BankTest.java
+++ src/test/java/com/booleanuk/extension/BankTest.java
@@ -1,4 +1,22 @@
 package com.booleanuk.extension;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankTest {
+
+    @Test
+    public void getBranchWhichExists() {
+        Branch branch = Bank.getBranch("WCA");
+
+        assertEquals(
+                "Warsaw",
+                branch.city()
+        );
+        assertEquals(
+                400000,
+                branch.overdraftLimit()
+        );
+    }
 }

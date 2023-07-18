79b8337f4f379ef18d78f741faf3add0d12a59a3
Pawel Kwiecinski <pawel.kwiecinski@talan.com>
2023-07-18 11:34:55 +0200

    green stage of showAccountHistoryTest

--- src/main/java/com/booleanuk/core/BankAccount.java
+++ src/main/java/com/booleanuk/core/BankAccount.java
@@ -3,6 +3,8 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class BankAccount {
     LinkedHashMap<String, List<Transaction>> accounts;
@@ -19,4 +21,29 @@ public class BankAccount {
         accounts.get(accountName).add(transaction);
         return true;
     }
+
+    public String showAccountHistory(String accountName) {
+        if(!accounts.containsKey(accountName))
+            return "accountName is not matching with any account";
+
+        List<Transaction> account = accounts.get(accountName);
+        StringBuilder result = new StringBuilder();
+        result.append("date       || credit  || debit  || balance\n");
+        AtomicReference<Float> balance = new AtomicReference<>(0f);
+        String transactionsString = account.stream().map(transaction -> {
+                balance.updateAndGet(currentBalance -> currentBalance + transaction.amount);
+                return rightPad(transaction.date.toString(), 10)
+                        + " || "
+                        + (transaction.amount > 0 ?
+                        rightPad(Float.toString(transaction.amount), 8) + "||" + " ".repeat(8)
+                        : " ".repeat(8) + "|| " + rightPad(Float.toString(-transaction.amount), 7))
+                        + "|| " + balance;}
+        ).collect(Collectors.joining("\n"));
+
+        result.append(transactionsString);
+        return result.toString();
+    }
+
+    private String leftPad(String text, int length) { return String.format("%" + length + "." + length + "s", text); }
+    private String rightPad(String text, int length) { return String.format("%-" + length + "." + length + "s", text); }
 }

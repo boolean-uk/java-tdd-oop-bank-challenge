fbab3a53fc99d9f11144382be8c666bddec3b34e
Kamil Kulak <kamil.kulak@talan.com>
2023-07-18 12:47:39 +0200

    Red stage for Deposit

--- src/test/java/com/booleanuk/core/TransfersTest.java
+++ src/test/java/com/booleanuk/core/TransfersTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-public class TransferTest {
+public class TransfersTest {
     private Transfer transfer;
+    private Deposit deposit;
+
     @BeforeEach
     public void setup() {
         transfer = new Transfer(30000, LocalDate.parse("2023-07-18"));
+        deposit = new Deposit(30000, LocalDate.parse("2023-07-18"));
     }
 
     @Test
@@ -27,5 +30,21 @@ public class TransferTest {
         Assertions.assertEquals(2023, transfer.getDate().getYear());
         Assertions.assertEquals(8, transfer.getDate().getMonth().getValue());
         Assertions.assertEquals(19, transfer.getDate().getDayOfMonth());
+    }
+
+    @Test
+    public void depositGetterAndSetterShouldReturnProperValue() {
+        Assertions.assertEquals(30000, deposit.getAmountOfMoney());
+        Assertions.assertEquals(2023, deposit.getDate().getYear());
+        Assertions.assertEquals(7, deposit.getDate().getMonth().getValue());
+        Assertions.assertEquals(18, deposit.getDate().getDayOfMonth());
+
+        deposit.setAmountOfMoney(40000);
+        deposit.setDate(LocalDate.parse("2023-08-19"));
+
+        Assertions.assertEquals(40000, deposit.getAmountOfMoney());
+        Assertions.assertEquals(2023, deposit.getDate().getYear());
+        Assertions.assertEquals(8, deposit.getDate().getMonth().getValue());
+        Assertions.assertEquals(19, deposit.getDate().getDayOfMonth());
     }
 }

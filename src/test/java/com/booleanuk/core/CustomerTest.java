bd518b88c6b4e0c8f3b94e813a42e491ab4057e6
Martyna <martyna.marszalek@talan.com>
2023-07-18 12:44:55 +0200

    Tests for customer's class methods: createCurrentAccount and createSavingAccount

--- src/test/java/com/booleanuk/core/CustomerTest.java
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -1,4 +1,27 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CustomerTest {
+
+    @Test
+    public void shouldCreateCurrentAccount() {
+        int customerId = 1;
+        Customer customer = new Customer(customerId);
+
+        Assertions.assertTrue(customer.createCurrentAccount("current", 1000));
+        Assertions.assertEquals(1, customer.getAccounts().size());
+        Assertions.assertEquals("current", customer.getAccounts().get(0).getType());
+    }
+
+    @Test
+    public void shouldCreateSavingsAccount() {
+        int customerId = 1;
+        Customer customer = new Customer(customerId);
+
+        Assertions.assertTrue(customer.createSavingsAccount("savings", 2000));
+        Assertions.assertEquals(1, customer.getAccounts().size());
+        Assertions.assertEquals("savings", customer.getAccounts().get(0).getType());
+    }
 }

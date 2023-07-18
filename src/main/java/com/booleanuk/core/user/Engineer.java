d890a1d89d960305d463c7fcf188b54f04e6354f
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-18 14:49:41 +0200

    Add Engineer calculating balance - logic implementation

--- src/main/java/com/booleanuk/core/user/Engineer.java
+++ src/main/java/com/booleanuk/core/user/Engineer.java
@@ -1,7 +1,38 @@
 package com.booleanuk.core.user;
 
-public class Engineer extends User {
+import com.booleanuk.core.banking.BankTransaction;
+import com.booleanuk.core.banking.TransactionType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+public class Engineer extends Customer {
+
+    private BigDecimal balance;
+
+
     public Engineer() {
         super();
+    }
+
+    public BigDecimal calculateBalanceFromHistory() {
+        BigDecimal internalBalance = BigDecimal.ZERO;
+        List<BankTransaction> transactions = getCurrentAccount().getTransactions();
+
+        for (BankTransaction transaction : transactions) {
+            if (transaction.getTransactionType().equals(TransactionType.WITHDRAW)) {
+                internalBalance = internalBalance.subtract(transaction.getAmount());
+            }
+            if (transaction.getTransactionType().equals(TransactionType.DEPOSIT)) {
+                internalBalance = internalBalance.add(transaction.getAmount());
+            }
+
+        }
+
+        return internalBalance;
     }
 }

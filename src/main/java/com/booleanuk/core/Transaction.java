37243a145de31df007944034bc7db01a65aefa94
Esther <estherbuijnsters@gmail.com>
2023-09-21 17:44:10 +0200

    Created setDateTest for Transaction

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,4 +1,17 @@
 package com.booleanuk.core;
 
 public class Transaction {
+
+    private String date;
+    private double credit;
+    private double debit;
+    private double balance;
+
+    public Transaction(String date, double credit, double debit, double balance) {
+        this.date = date;
+        this.credit = credit;
+        this.debit = debit;
+        this.balance = balance;
+
+    }
 }

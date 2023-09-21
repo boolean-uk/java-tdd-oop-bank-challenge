910395f683230dd5ec718c9ff8514333e6ad78d6
Esther <estherbuijnsters@gmail.com>
2023-09-21 16:30:20 +0200

    Created depositTest and passed getBalance

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
 public class Account {
+
+    private double accountNumber;
+    private double balance;
+
+    public Account(double accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -12,4 +12,13 @@ public class AccountTest {
         Assertions.assertEquals(500.00, account.getBalance());
 
     }
+
+    @Test
+    public void testDepositAmountWorks() {
+        Account account = new Account(123456, 500.00);
+        account.depositAmount(150.00);
+
+        Assertions.assertEquals(500.00, account.getBalance(), 0.01);
+        Assertions.assertEquals(650.00, account.getNewBalance(), 0.01);
+    }
 }

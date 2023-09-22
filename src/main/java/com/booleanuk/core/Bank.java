71321ec88511a0d6107fb3559d33acee8d5707ac
Esther <estherbuijnsters@gmail.com>
2023-09-22 11:32:14 +0200

    Created creatingAccounts Tests

--- src/main/java/com/booleanuk/core/Bank.java
+++ src/main/java/com/booleanuk/core/Bank.java
@@ -1,4 +1,21 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
+
+    private String accountNumber;
+    private double balance;
+    private String accountType;
+    private List<Bank> accounts;
+
+    public Bank() {
+        this.accountType = accountType;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.accounts = new ArrayList<>();
+    }
+
+
 }
--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -1,15 +1,29 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTest {
 
     @Test
+    public void testCreateSavingsAccountWorks() {
+        Bank bank = new Bank();
+        bank.createAccount("SA12345", 1000.00, "SavingsAccount");
 
-    public void testSetCurrentAccountWorks() {
-        Bank bank = new bank();
-        Account account = new account();
+        assertEquals("SavingsAccount", bank.getAccountType("SA12345"));
+        bank.createAccount("SA234567", 1000.0, "SavingsAccount");
+        Assertions.assertEquals("SavingsAccount", bank.getAccountType("SA234567"));
+    }
 
+    @Test
+    public void testCreateCurrentAccountWorks() {
+        Bank bank = new Bank();
+        bank.createAccount("CA12345", 1000.00, "CurrentAccount");
 
-
+        assertEquals("CurrentAccount", bank.getAccountType("CA12345"));
+        bank.createAccount("CA234567", 1000.0, "CurrentAccount");
+        Assertions.assertEquals("CurrentAccount", bank.getAccountType("CA234567"));
+    }
 }

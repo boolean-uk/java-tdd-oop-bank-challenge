a14d0a08428cfd67b65a73373d4d3db01380410c
Esther <estherbuijnsters@gmail.com>
2023-09-22 14:13:09 +0200

    Created more tests for creatingAccount

--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class BankTest {
 
@@ -26,4 +27,38 @@ public class BankTest {
         Assertions.assertTrue(bank.createAccount("CA234567", 1000.0, "CurrentAccount"));
         Assertions.assertEquals("CurrentAccount", bank.getAccountType("CA234567"));
     }
+
+    @Test
+    public void testCreateCurrentAccountFailsOnAccountType() {
+        Bank bank = new Bank();
+
+        Assertions.assertFalse(bank.createAccount("CA12345", 1000.0, "Account"));
+        assertNull(bank.getAccountType("CA12345"));
+    }
+
+    @Test
+    public void testCreateSavingsAccountFailsOnAccountType() {
+        Bank bank = new Bank();
+
+        Assertions.assertFalse(bank.createAccount("SA12345", 1000.0, "Account"));
+        assertNull(bank.getAccountType("SA12345"));
+    }
+    @Test
+    public void testCreateCurrentAccountFailsItAlreadyExists() {
+        Bank bank = new Bank();
+        bank.createAccount("CA12345", 1000.00, "CurrentAccount");
+
+        assertEquals("CurrentAccount", bank.getAccountType("CA12345"));
+        Assertions.assertFalse(bank.createAccount("CA12345", 1000.00, "CurrentAccount"));
+    }
+
+    @Test
+    public void testCreateSavingsAccountFailsItAlreadyExists() {
+        Bank bank = new Bank();
+        bank.createAccount("SA12345", 1000.00, "SavingsAccount");
+
+        assertEquals("SavingsAccount", bank.getAccountType("SA12345"));
+        Assertions.assertFalse(bank.createAccount("SA234567", 1000.0, "SavingsAccount"));
+    }
 }
+

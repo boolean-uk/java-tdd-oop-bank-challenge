eba4d75797898b2e9f8da6c14e93a5fcb4698b5e
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-24 23:51:44 +0200

    modified timestamp comparator

--- src/main/java/com/booleanuk/core/Bank/TransactionTimestampComparator.java
+++ src/main/java/com/booleanuk/core/Bank/TransactionTimestampComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class TransactionTimestampComparator implements Comparator<Transaction> {
     @Override
     public int compare(Transaction t1, Transaction t2) {
-        return t2.getTimestamp().compareTo(t1.getTimestamp());
+        return t1.getTimestamp().compareTo(t2.getTimestamp());
     }
 }

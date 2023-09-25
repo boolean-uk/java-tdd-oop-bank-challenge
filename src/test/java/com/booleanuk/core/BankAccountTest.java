e9d56175eca11814d1958e272a22a84ea4c38fef
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-25 02:19:20 +0200

    Added accounttype Test
    Added a test to see if the user can make a savingsaccount & currentaccount

---
+++ src/test/java/com/booleanuk/core/BankAccountTest.java
@@ -0,0 +1,20 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    @Test
+    public void userCreatesAccountTypes() {
+        CurrentAccount currentAccount = new CurrentAccount();
+        String banktype1 = currentAccount.getBankType();
+        Assertions.assertEquals("current", banktype1);
+
+        SavingsAccount savingsaccount = new SavingsAccount();
+        String banktype2 = savingsaccount.getBankType();
+        Assertions.assertEquals("savings", banktype2);
+    }
+
+
+}

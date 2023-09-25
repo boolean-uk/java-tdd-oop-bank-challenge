d2bfa4dccd9df699247d83213565c99b4bebdf70
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-25 03:46:45 +0200

    Added date test for savingsaccount & added test for transfertable

--- src/test/java/com/booleanuk/core/BankAccountTest.java
+++ src/test/java/com/booleanuk/core/BankAccountTest.java
@@ -64,12 +64,51 @@ public class BankAccountTest {
 
         SimpleDateFormat datePatternSetter = new SimpleDateFormat("dd/MM/yyyy");
         Date date = new Date();
-        String currentdateformatter = datePatternSetter.format(date);
+        String currentDateFormatter = datePatternSetter.format(date);
 
-        Assertions.assertEquals(currentdateformatter, dateFromClass);
+        Assertions.assertEquals(currentDateFormatter, dateFromClass);
+
+        SavingsAccount savingsaccount = new SavingsAccount(5000);
+        String dateFromClass2 = savingsaccount.getDate();
+
+        SimpleDateFormat datePatternSetter2 = new SimpleDateFormat("dd/MM/yyyy");
+        Date date2 = new Date();
+        String currentDateFormatterTwo = datePatternSetter2.format(date2);
+
+        Assertions.assertEquals(currentDateFormatterTwo, dateFromClass2);
 
 
     }
 
+    @Test
+    public void checkTranferTable() {
+        double startBalance = 5000;
+        CurrentAccount currentAccount = new CurrentAccount(startBalance);
+        double depositOne = 1200.00;
+        double depositTwo = 400.00;
+        double depositThree = 800.00;
 
+        double withdrawOne = 600.00;
+        double withdrawTwo = 200.00;
+
+        //Deposit 1
+        currentAccount.deposit(depositOne);
+        //Withdraw 1
+        currentAccount.withdraw(withdrawOne);
+        //Deposit 2 & 3
+        currentAccount.deposit(depositTwo);
+        currentAccount.deposit(depositThree);
+        //Withdraw 2
+        currentAccount.withdraw(withdrawTwo);
+
+        double newBalanceTest = startBalance + depositOne + depositTwo + depositThree - withdrawOne - withdrawTwo;
+
+        Assertions.assertEquals(newBalanceTest, currentAccount.balance);
+        /*
+        Didn't really know how to test for the table output
+        The test checks for the output of the withdrawn + deposited amounts.
+        I will show the table as a System.out.println
+                 */
+
+    }
 }

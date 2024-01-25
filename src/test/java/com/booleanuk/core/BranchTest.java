c1dd322618aab1671a35d856684fb8ba9e44b464
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 14:30:17 +0100

    Create test for overdraftRequest Map in Branch

--- src/test/java/com/booleanuk/core/BranchTest.java
+++ src/test/java/com/booleanuk/core/BranchTest.java
@@ -13,5 +13,16 @@ class BranchTest {
         Assertions.assertEquals("1", branch.getBranchId());
         Assertions.assertEquals("DNC", branch.getName());
         Assertions.assertEquals("London", branch.getLocation());
+        Assertions.assertTrue(branch.getOverdraftRequests().isEmpty());
+    }
+
+    @Test
+    public void testBranchOverdraftMapInitialized() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Branch branch = new Branch("1", "DNC", "London");
+        Account account = new Account("1", customer, branch);
+        branch.getOverdraftRequests().put(account, 500.0);
+        Assertions.assertFalse(branch.getOverdraftRequests().isEmpty());
+        Assertions.assertEquals(500.0,branch.getOverdraftRequests().get(account));
     }
 }

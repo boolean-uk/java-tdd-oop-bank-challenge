5ebc3c8e3ce1032ab723a89fadfe2d55baba582c
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-25 14:18:34 +0100

    empty methods added

--- src/main/java/com/booleanuk/extension/CurrentAccount.java
+++ src/main/java/com/booleanuk/extension/CurrentAccount.java
@@ -1,7 +1,17 @@
 package com.booleanuk.extension;
 
 public class CurrentAccount extends Account {
+    double overDraftLimit;
     public CurrentAccount() {
+        this.overDraftLimit = 0;
+    }
 
+    boolean setOverDraftLimit(){
+        return false;
+    }
+
+    @Override
+    public boolean add(double amount){
+        return false;
     }
 }

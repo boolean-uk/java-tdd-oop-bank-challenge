3862d972695f876503920d37803a7897c87e98d9
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:37:36 +0100

    Create test for transactions list implemented in Account

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -3,6 +3,8 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
@@ -14,6 +16,15 @@ class AccountTest {
         Assertions.assertEquals("1", account.getAccountId());
         Assertions.assertEquals(customer, account.getCustomer());
         Assertions.assertEquals(0.0, account.getBalance());
+        Assertions.assertTrue(account.getTransactions().isEmpty());
+    }
+
+    @Test
+    public void testTransactionListInstantiated() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Account account = new Account("1", customer);
+        account.getTransactions().add(new Transaction(new Date(),100, 1000));
+        Assertions.assertFalse(account.getTransactions().isEmpty());
     }
 
 }

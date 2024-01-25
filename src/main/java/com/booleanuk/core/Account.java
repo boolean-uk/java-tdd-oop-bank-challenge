65fba95760b650a2c37993249fee403c0d094927
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:26:19 +0100

    Implement Account class

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,4 +1,23 @@
 package com.booleanuk.core;
 
 public class Account {
+    private String accountId;
+    private Customer customer;
+    private double balance;
+
+    public Account(String accountId, Customer customer) {
+        this.accountId = accountId;
+        this.customer = customer;
+        this.balance = 0.0;
+    }
+
+    public String getAccountId() {
+        return this.accountId;
+    }
+    public Customer getCustomer() {
+        return this.customer;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
 }

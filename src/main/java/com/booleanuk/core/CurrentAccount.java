6d52a61565678c86d5b7e67ce95819dca4059b67
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:16:24 +0100

    Implement CurrentAccount class

--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -1,4 +1,23 @@
 package com.booleanuk.core;
 
 public class CurrentAccount {
+    private String accountId;
+    private Customer customer;
+    private double balance;
+
+    public CurrentAccount(String accountId, Customer customer) {
+        this.accountId = accountId;
+        this.customer = customer;
+        this.balance = 0.0;
+    }
+
+    public String getAccountId() {
+        return this.accountId;
+    }
+    public Customer getCustomer() {
+        return this.customer;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
 }

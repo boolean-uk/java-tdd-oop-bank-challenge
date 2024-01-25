e8f3ba529d0718ebc782617b4f65b2c871859f05
Marit <marit.moe2@ntebb.no>
2024-01-25 10:16:59 +0100

    Transaction tests.

--- src/test/java/com/booleanuk/core/TransactionTest.java
+++ src/test/java/com/booleanuk/core/TransactionTest.java
@@ -3,24 +3,22 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TransactionTest {
 
     @Test
-    void getDateTimeWithoutNano() {
+    public void getDateTimeWithoutNano() {
         Transaction transaction = new Transaction(100, 100);
         Assertions.assertEquals(transaction.dateTime.minusNanos(transaction.dateTime.getNano()), transaction.getDateTime());
     }
 
     @Test
-    void getAmount() {
+    public void getAmount() {
         Transaction transaction = new Transaction(100, 100);
         Assertions.assertEquals(100, transaction.getAmount());
     }
 
     @Test
-    void getBalance() {
+    public void getBalance() {
         Transaction transaction = new Transaction(100, 100);
         Assertions.assertEquals(100, transaction.getBalance());
     }

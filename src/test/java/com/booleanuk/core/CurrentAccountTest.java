7b407572a75d845ffc830be465cbebd97a967b96
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:15:52 +0100

    Create CurrentAccount constructor test

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -1,7 +1,19 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CurrentAccountTest {
+
+    @Test
+    public void testCustomerConstructor() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        CurrentAccount currentAccount = new CurrentAccount("1", customer);
+        Assertions.assertEquals("1", currentAccount.getAccountId());
+        Assertions.assertEquals(customer, currentAccount.getCustomer());
+        Assertions.assertEquals(0.0, currentAccount.getBalance());
+    }
 
 }

105f65edb117d9f104655b44af0959fa56c0dc3d
Sebastian <sebastian.thunberg@hotmail.se>
2024-01-25 15:23:33 +0100

    set positive requirement for setOverdraftLimit

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -92,8 +92,9 @@ class AccountCurr extends Account {
 	public void setOverdraftEnabled(boolean overdraftEnabled) {
 		this.overdraftEnabled = overdraftEnabled;
 	}
-	public void setOverdraftLimit(int limit){
-		this.overdraftLimit=limit;
+
+	public void setOverdraftLimit(int limit) {
+		this.overdraftLimit = Math.max(limit, 0);
 	}
 }
 

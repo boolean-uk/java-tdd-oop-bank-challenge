80e697004cd46f44a591351d7da5f4123fcf6abd
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:33:06 +0100

    Create test for transaction constructor

--- src/test/java/com/booleanuk/core/TransactionTest.java
+++ src/test/java/com/booleanuk/core/TransactionTest.java
@@ -1,7 +1,20 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
+
+    @Test
+    public void testTransactionConstructor() {
+        Transaction transaction = new Transaction(new Date(), 100, 1000);
+        Assertions.assertEquals(new Date(), transaction.getDate());
+        Assertions.assertEquals(100, transaction.getAmount());
+        Assertions.assertEquals(1000, transaction.getBalance());
+    }
 
 }

1eaacdeb332ff9653296e9d06e40aeb37681fb59
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:33:35 +0100

    Implement transaction class

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,4 +1,27 @@
 package com.booleanuk.core;
 
+import java.util.Date;
+
 public class Transaction {
+    private Date date;
+    private double amount;
+    private double balance;
+
+    public Transaction(Date date, double amount, double balance) {
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
 }

66a1387eb262cda41b0af717852b5f07be65c7b4
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-25 14:28:19 +0100

    Add test for withdrawMoreThanNullOverdraft

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -1,5 +1,6 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CurrentAccountTest {
@@ -16,13 +17,26 @@ public class CurrentAccountTest {
 
         Customer customer = new Customer();
 
-        Account currentAccount = new CurrentAccount("AccountName", Branches.Oslo);
+        CurrentAccount currentAccount = new CurrentAccount("AccountName", Branches.Oslo);
+
+        BankManager bankManager = new BankManager();
 
         customer.createAccount(currentAccount);
 
         Overdraft overdraft = new Overdraft(500);
+        Overdraft overdraft2 = new Overdraft(400);
 
 
+        customer.requestOverdraft(currentAccount, bankManager, overdraft);
+
+        bankManager.rejectOverdraft(overdraft);
+
+        Assertions.assertFalse(customer.withdraw(currentAccount, 400));
+
+        bankManager.addOverDraft(overdraft2);
+        bankManager.approveOverdraft(overdraft2);
+
+        Assertions.assertTrue(customer.withdraw(currentAccount, 400));
 
     }
 

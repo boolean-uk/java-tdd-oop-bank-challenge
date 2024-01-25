a1d5c90cf1155b5474e2c2a7856c9d624a2f0035
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-25 09:42:43 +0100

    Created test for user story 2

--- src/test/java/com/booleanuk/core/SavingsAccountTest.java
+++ src/test/java/com/booleanuk/core/SavingsAccountTest.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SavingsAccountTest {
+
+    //User story 2
+    @Test
+    public void assertSavingsAccountWasCreated() {
+        int accountId = 123;
+        SavingsAccount savingsAccount = new SavingsAccount(accountId);
+        Assertions.assertEquals(accountId, savingsAccount.getAccountId());
+        Assertions.assertEquals(0.0, savingsAccount.getBalance());
+    }
 }

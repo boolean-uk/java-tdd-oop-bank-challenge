31936f8c5854673562ca5590118d656b8a57b609
Marit <marit.moe2@ntebb.no>
2024-01-25 14:19:31 +0100

    Skeleton CurrentAccount added overdraft functionality.

--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -1,7 +1,22 @@
 package com.booleanuk.core;
 
 public class CurrentAccount extends Account{
+    private boolean overdraftRequested;
+    private double overdraftAmount;
+
     public CurrentAccount(User owner) {
         super(owner);
+        this.overdraftRequested = false;
+        this.overdraftAmount = 0.0;
     }
+
+    public boolean requestOverdraft() {
+        return false;
+    }
+
+    public void answearOverdraftRequest(boolean confirm, double overdraftAmount) {
+
+    }
+
+
 }

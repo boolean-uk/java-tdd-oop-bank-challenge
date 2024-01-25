8b39ba89103d116f44dc68429a8d5d47f2db61aa
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-25 09:44:14 +0100

    Implemented constructor method for user story 2

--- src/main/java/com/booleanuk/core/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/SavingsAccount.java
@@ -1,4 +1,7 @@
 package com.booleanuk.core;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account{
+    public SavingsAccount(int accountId) {
+        super(accountId);
+    }
 }

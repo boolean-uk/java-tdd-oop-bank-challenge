2eb09d9beaf5c39f0a261709644a915c1a9a6802
Marit <marit.moe2@ntebb.no>
2024-01-25 10:14:34 +0100

    Skeleton Account class.

---
+++ src/main/java/com/booleanuk/core/Account.java
@@ -0,0 +1,46 @@
+package com.booleanuk.core;
+
+import java.util.ArrayList;
+
+public class Account {
+    int accountNumber;
+    User owner;
+    int balance;
+    ArrayList<Transaction> transactions;
+
+    public Account(User owner) {
+        this.owner = owner;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+}

9736f7ba6530261fce33bf9f195ba5c390e28651
noahlenn <noahlenn@hotmail.com>
2024-01-25 09:47:08 +0100

    BankChallengeTest 2

--- src/test/java/com/booleanuk/core/BankChallengeTest.java
+++ src/test/java/com/booleanuk/core/BankChallengeTest.java
@@ -7,14 +7,46 @@ public class BankChallengeTest {
 
     @Test
     public void createCurrentAccount() {
-        CurrentAccount current = new CurrentAccount();
+        CurrentAccount current1 = new CurrentAccount();
+        CurrentAccount current2 = new CurrentAccount(10.0);
 
-        Assertions.assertNotNull(current);
+        Assertions.assertNotNull(current1);
+        Assertions.assertNotNull(current2);
+
     }
     @Test
     public void currentAccountBalance() {
-        CurrentAccount current = new CurrentAccount();
+        CurrentAccount current1 = new CurrentAccount();
+        CurrentAccount current2 = new CurrentAccount(10.0);
+        CurrentAccount current3 = new CurrentAccount();
 
-        Assertions.assertEquals(0.0, current.getBalance());
+        current3.setBalance(100.0);
+
+        Assertions.assertEquals(0.0, current1.getBalance());
+        Assertions.assertEquals(10.0, current2.getBalance());
+        Assertions.assertEquals(100.0, current3.getBalance());
+    }
+
+
+    @Test
+    public void createSavingsAccount() {
+        SavingsAccount savings1 = new SavingsAccount();
+        SavingsAccount savings2 = new SavingsAccount(10.0);
+
+        Assertions.assertNotNull(savings1);
+        Assertions.assertNotNull(savings2);
+
+    }
+    @Test
+    public void savingsAccountBalance() {
+        SavingsAccount savings1 = new SavingsAccount();
+        SavingsAccount savings2 = new SavingsAccount(10.0);
+        SavingsAccount savings3 = new SavingsAccount();
+
+        savings3.setBalance(100.0);
+
+        Assertions.assertEquals(0.0, savings1.getBalance());
+        Assertions.assertEquals(10.0, savings2.getBalance());
+        Assertions.assertEquals(100.0, savings3.getBalance());
     }
 }

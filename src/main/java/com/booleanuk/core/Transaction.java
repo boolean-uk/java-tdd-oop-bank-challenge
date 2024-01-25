20e86a8fec16cb9d3d56cb22358734406206a26a
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-25 10:19:05 +0100

    Test for account class written and transactions class added

---
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -0,0 +1,15 @@
+package com.booleanuk.core;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Transaction {
+    String date;
+    double amount;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public Transaction(double amount) {
+        this.date = java.time.LocalDate.now().format(formatter);
+        this.amount = amount;
+    }
+}
--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,9 +1,58 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
+    @Test
+    public void testAdd(){
+        Account account = new SavingsAccount();
 
+        //Add
+        Assertions.assertEquals(0, account.getBalance());
+        account.add(50);
+        Assertions.assertEquals(50, account.getBalance());
+
+    }
+
+    @Test
+    public void testRemove(){
+        Account account = new SavingsAccount();
+
+        account.add(500);
+        account.remove(200);
+        Assertions.assertEquals(300, account.getBalance());
+
+    }
+
+    @Test
+    public void testGetTransactions(){
+        Account account = new SavingsAccount();
+        account.add(500);
+        account.remove(200);
+        Assertions.assertEquals(300, account.getBalance());
+
+        List<Transaction> testList = new ArrayList<>();
+        testList.add(new Transaction(500));
+        testList.add(new Transaction(-200));
+
+        Assertions.assertEquals(testList, account.getTransactions());
+    }
+
+    @Test
+    public void testGetAccountNr(){
+        Account account = new SavingsAccount();
+        Account account2 = new CurrentAccount();
+        Assertions.assertEquals(1, account.getAccountNr());
+        Assertions.assertEquals(2, account2.getAccountNr());
+    }
 
 }

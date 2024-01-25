65602e727a61272a1027db82b5ab8aa4e10180be
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 11:00:00 +0100

    Create tests for getBankStatement()

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -23,8 +23,32 @@ class AccountTest {
     public void testTransactionListInstantiated() {
         Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
         Account account = new Account("1", customer);
-        account.getTransactions().add(new Transaction(new Date(),100, 1000));
+        account.getTransactions().add(new Transaction(new Date(),100, TransactionType.WITHDRAW ,1000));
         Assertions.assertFalse(account.getTransactions().isEmpty());
     }
 
+    @Test
+    public void testGetBankStatementWithTransaction() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Account account = new Account("1", customer);
+        account.getTransactions().add(new Transaction(new Date(),100, TransactionType.DEPOSIT, 100));
+        Assertions.assertEquals("", account.getBankStatement());
+    }
+
+    @Test
+    public void testGetBankStatementWithMultipleTransactions() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Account account = new Account("1", customer);
+        account.getTransactions().add(new Transaction(new Date(),200, TransactionType.WITHDRAW, 900));
+        account.getTransactions().add(new Transaction(new Date(),100, TransactionType.DEPOSIT, 1100));
+        account.getTransactions().add(new Transaction(new Date(),1000, TransactionType.DEPOSIT, 1000));
+        Assertions.assertEquals("", account.getBankStatement());
+    }
+
+    @Test
+    public void testGetBankStatementWithoutTransaction() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Account account = new Account("1", customer);
+        Assertions.assertEquals("No transactions", account.getBankStatement());
+    }
 }

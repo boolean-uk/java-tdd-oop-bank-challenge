aa1aa8e3e0a69570acc89c2ecea007ed0923d64c
Marit <marit.moe2@ntebb.no>
2024-01-25 10:37:51 +0100

    Pass test createUser().

--- src/main/java/com/booleanuk/core/User.java
+++ src/main/java/com/booleanuk/core/User.java
@@ -1,4 +1,27 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class User {
+    int userId;
+    String name;
+    ArrayList<Account> accounts;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+        this.accounts = new ArrayList<>();
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
 }

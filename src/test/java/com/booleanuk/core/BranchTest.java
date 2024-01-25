1e0635ac7960d6e69642e26d3da381e066280db4
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 15:05:01 +0100

    Create tests for reviewOverdraftRequest

--- src/test/java/com/booleanuk/core/BranchTest.java
+++ src/test/java/com/booleanuk/core/BranchTest.java
@@ -26,4 +26,35 @@ class BranchTest {
         Assertions.assertFalse(branch.getOverdraftRequests().isEmpty());
         Assertions.assertEquals(500.0,branch.getOverdraftRequests().get(account));
     }
+
+    @Test
+    public void testDeclineOverdraftRequest() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Branch branch = new Branch("1", "DNC", "London");
+        Account account = new Account("1", customer, branch);
+
+        branch.addOverdraftRequest(account, 500.0);
+
+        Assertions.assertEquals("Request for account 1 declined",branch.reviewOverdraftRequest("1",false));
+    }
+
+    @Test
+    public void testAcceptOverdraftRequest() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Branch branch = new Branch("1", "DNC", "London");
+        Account account = new Account("1", customer, branch);
+
+        branch.addOverdraftRequest(account, 500.0);
+
+        Assertions.assertEquals(0, account.getOverdraftLimit());
+        Assertions.assertEquals("Request for account 1 accepted",branch.reviewOverdraftRequest("1",true));
+        Assertions.assertEquals(500, account.getOverdraftLimit());
+    }
+
+    @Test
+    public void testReviewNoneExistingOverdraftRequest() {
+        Branch branch = new Branch("1", "DNC", "London");
+
+        Assertions.assertEquals("No request found",branch.reviewOverdraftRequest("1",true));
+    }
 }

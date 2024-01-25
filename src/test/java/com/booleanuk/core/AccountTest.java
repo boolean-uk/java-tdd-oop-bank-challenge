255160b35a910d097990c2f2dec725cc9746224b
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:25:54 +0100

    Create test for Account class

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,7 +1,19 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+
+    @Test
+    public void testCurrentAccountConstructor() {
+        Customer customer = new Customer("1", "Java Man", "12345678", "java@man.coder");
+        Account account = new Account("1", customer);
+        Assertions.assertEquals("1", account.getAccountId());
+        Assertions.assertEquals(customer, account.getCustomer());
+        Assertions.assertEquals(0.0, account.getBalance());
+    }
 
 }

274cf7dd456aa83e19ccfe6e30f8b8b654900503
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 14:31:15 +0100

    Add overdraftRequest Map in Branch

--- src/main/java/com/booleanuk/core/Branch.java
+++ src/main/java/com/booleanuk/core/Branch.java
@@ -1,16 +1,24 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Branch {
     private String branchId;
     private String name;
     private String location;
+    private Map<Account, Double> overdraftRequests;
 
     public Branch(String branchId, String name, String location) {
         this.branchId = branchId;
         this.name = name;
         this.location = location;
+        this.overdraftRequests = new HashMap<>();
     }
 
+    public Map<Account, Double> getOverdraftRequests() {
+        return this.overdraftRequests;
+    }
     public String getBranchId() {
         return this.branchId;
     }

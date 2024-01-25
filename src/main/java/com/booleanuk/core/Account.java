a41bed1232d6fc61db2605efe659dcc64e625680
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 10:37:53 +0100

    Implement transactions list in Account

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,14 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
     private String accountId;
     private Customer customer;
     private double balance;
+    private List<Transaction> transactions;
 
     public Account(String accountId, Customer customer) {
         this.accountId = accountId;
         this.customer = customer;
         this.balance = 0.0;
+        this.transactions = new ArrayList<>();
     }
 
     public String getAccountId() {
@@ -19,5 +24,8 @@ public class Account {
     }
     public double getBalance() {
         return this.balance;
+    }
+    public List<Transaction> getTransactions() {
+        return transactions;
     }
 }

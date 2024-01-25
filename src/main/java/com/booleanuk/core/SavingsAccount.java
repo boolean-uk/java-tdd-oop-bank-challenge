ce025a5df23784c7640797f2bdef441d75fe5a68
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-25 14:48:32 +0100

    Create overwritten RequestOverdraft method

--- src/main/java/com/booleanuk/core/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/SavingsAccount.java
@@ -5,4 +5,9 @@ public class SavingsAccount extends Account {
     public SavingsAccount(String accountId, Customer customer, Branch branch) {
         super(accountId, customer, branch);
     }
+
+    @Override
+    public String requestOverdraft(double amount) {
+        return "You can not overdraft a savings account";
+    }
 }

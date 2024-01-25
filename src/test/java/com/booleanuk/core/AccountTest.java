f09aa11a0323cf7367fbb039b9491ea433d87bc4
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-25 10:01:58 +0100

    Created test for user story 3

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Test;
+
 public class AccountTest {
+
+    //User story 3
+    @Test
+    public void generatedBankStatementShouldHaveAllTransactions() {
+        Account account = new Account(123);
+        account.deposit(100);
+        account.withdraw(50);
+        String actualBankStatement = account.getBankStatement();
+    }
 }

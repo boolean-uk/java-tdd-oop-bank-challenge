6b125a957b67a4a316a9adefac9ccefca9298a2c
Alexander <alexander.ellnestam@gmail.com>
2024-02-02 15:21:13 +0100

    Test pass

--- src/main/java/com/booleanuk/core/CurrentCustomerAccount.java
+++ src/main/java/com/booleanuk/core/CurrentCustomerAccount.java
@@ -1,4 +1,7 @@
 package com.booleanuk.core;
 
-public class CurrentCustomerAccount {
+public class CurrentCustomerAccount extends Customer{
+    public CurrentCustomerAccount(String accountNum, String name, String branchNum) {
+        super(accountNum, name, branchNum);
+    }
 }

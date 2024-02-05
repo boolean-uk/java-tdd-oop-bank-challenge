168cb6a31d295ae087527e63162183feb3b589a7
Alexander <alexander.ellnestam@gmail.com>
2024-02-05 17:48:34 +0100

    New test

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,4 +1,17 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AccountTest {
+
+    @Test
+    public void createAccount(){
+
+        Account account = new Account();
+
+        Assertions.assertTrue(account.addAccount("Savings","1111-1111-1111", "Alexander Ellnestam", "ABC123"));
+        Assertions.assertTrue(account.addAccount("Current","1111-1111-1111", "Alexander Ellnestam", "ABC123"));
+    }
+
 }

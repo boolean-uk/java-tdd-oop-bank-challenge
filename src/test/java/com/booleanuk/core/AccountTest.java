7893d89bc47fd5c922f4d5dc6e4feb340321449d
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-22 14:39:52 +0200

    Account fail

---
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccountTest {
+
+    @Test
+    public void testCreateAccount() {
+        Account savings = new SavingsAccount(0, "Company");
+        assertEquals(0, savings.getId());
+    }
+}

8fa1ce15b0a119bd9496f8737813c7020b37a966
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-22 14:43:55 +0200

    Account pass

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,4 +1,43 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Account {
+
+    private int id;
+    private String type;
+    private String branch;
+    private ArrayList<Transaction> transactions;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
--- src/main/java/com/booleanuk/core/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/SavingsAccount.java
@@ -1,4 +1,13 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class SavingsAccount extends Account{
+
+    public SavingsAccount(int id, String branch) {
+        this.setId(id);
+        this.setType("Saving");
+        this.setBranch(branch);
+        this.setTransactions(new ArrayList<>());
+    }
 }

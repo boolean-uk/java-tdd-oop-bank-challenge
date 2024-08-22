9f27763ea8abae30dc1d7a9e360c67665088a489
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-22 15:16:51 +0200

    testGetBalance() pass

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -4,12 +4,18 @@ import java.util.UUID;
 
 public class Account {
     String accountNumber;
+    double balance;
 
     public Account(){
         this.accountNumber = UUID.randomUUID().toString();
+        this.balance = 0;
     }
 
     public String getAccountNumber() {
         return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
     }
 }

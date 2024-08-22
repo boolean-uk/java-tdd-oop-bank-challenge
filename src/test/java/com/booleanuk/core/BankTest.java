f29dbfcef26733545ab16ebff856a1b752a67de7
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-22 14:57:41 +0200

    added tests to testGetCustomers() pass

--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -23,8 +23,15 @@ public class BankTest {
     @Test
     public void testGetCustomers() {
         Bank bank = new Bank("Bank");
-        Customer customer = new Customer("Name");
-        bank.addCustomer(customer);
+        Customer customer1 = new Customer("Name");
+        Customer customer2 = new Customer("Name");
+        bank.addCustomer(customer1);
         Assertions.assertEquals(1, bank.getCustomers().size());
+        Assertions.assertTrue(bank.getCustomers().contains(customer1));
+
+        bank.addCustomer(customer2);
+        Assertions.assertEquals(2, bank.getCustomers().size());
+        Assertions.assertTrue(bank.getCustomers().contains(customer1));
+        Assertions.assertTrue(bank.getCustomers().contains(customer2));
     }
 }

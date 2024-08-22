066498d6f9fedf43b60345fb814b11fad07c396d
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-22 15:36:39 +0200

    testGetAccounts() fails

--- src/test/java/com/booleanuk/core/CustomerTest.java
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -18,4 +18,11 @@ public class CustomerTest {
         bank.addCustomer(customer);
         Assertions.assertTrue(customer.requestCurrentAccount());
     }
+
+    @Test
+    public void testGetAccounts() {
+        Bank bank = new Bank("Bank");
+        Customer customer = new Customer("Name");
+        Assertions.assertEquals(0, customer.getAccounts().size());
+    }
 }

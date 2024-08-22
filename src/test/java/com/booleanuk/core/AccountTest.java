729627a7738f50dd0f4aa860bde1be71b1962262
gremble0 <hermanstornes@gmail.com>
2024-08-22 11:06:02 +0200

    Make withdraw/deposit tests

---
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -0,0 +1,39 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+  @Test
+  public void testWithdraw() {
+    Account account = new CurrentAccount(100);
+
+    account.withdraw(50);
+    Assertions.assertEquals(50, account.balance());
+
+    account.withdraw(20);
+    Assertions.assertEquals(30, account.balance());
+  }
+
+  @Test
+  public void testDeposit() {
+    Account account = new CurrentAccount(100);
+
+    account.deposit(50);
+    Assertions.assertEquals(150, account.balance());
+
+    account.deposit(20);
+    Assertions.assertEquals(170, account.balance());
+  }
+
+  @Test
+  public void testWithdrawDeposit() {
+    Account account = new CurrentAccount(100);
+
+    account.withdraw(50);
+    Assertions.assertEquals(50, account.balance());
+
+    account.deposit(50);
+    Assertions.assertEquals(100, account.balance());
+  }
+}

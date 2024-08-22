611ada83d0d9826de01882e506926380bef9e908
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-22 14:47:06 +0200

    testAddCustomer() add customer second time should be false pass

--- src/main/java/com/booleanuk/core/Bank.java
+++ src/main/java/com/booleanuk/core/Bank.java
@@ -1,13 +1,22 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
     private final String name;
+    private List<Customer> customers;
 
     public Bank(String name) {
         this.name = name;
+        this.customers = new ArrayList<>();
     }
 
     public boolean addCustomer(Customer customer) {
+        if (customers.contains(customer)){
+            return false;
+        }
+        customers.add(customer);
         return true;
     }
 }

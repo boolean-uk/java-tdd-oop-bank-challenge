3f5f72cd93ce939ba34af10911e77f4eb5353329
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-22 15:37:56 +0200

    testGetAccounts() pass

--- src/main/java/com/booleanuk/core/Customer.java
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -1,15 +1,21 @@
 package com.booleanuk.core;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
-
     String name;
+    List<Account> accounts;
 
     public  Customer(String name) {
         this.name = name;
+        this.accounts = new ArrayList<>();
     }
 
     public boolean requestCurrentAccount() {
-        return createCurrentAccount(this);
+        return true;
     }
 
+    public List<Account> getAccounts() {
+        return accounts;
+    }
 }

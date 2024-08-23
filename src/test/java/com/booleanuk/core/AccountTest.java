645f9f6c44318c0e0101fcea4feea4c226de5650
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-23 11:50:30 +0200

    fixed the test so that it uses the child classes for the accounts

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 public class AccountTest {
 
     @Test
-    public void makeAccountTest() {
-        Account account = new Account(500);
+    public void makeCurrentAccountTest() {
+        Account account = new CurrentAccount(500);
         Assertions.assertNotNull(account, "The account should be created");
     }
 
     @Test
     public void checkBalance() {
-        Account account = new Account(500);
+        Account account = new CurrentAccount(500);
         Assertions.assertEquals(500, account.getBalance());
     }
 

2bf63d6d5b532d844608b5665882bef940ab0f08
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-23 14:29:34 +0200

    testRequestSavingsAccount() fails

--- src/test/java/com/booleanuk/core/CustomerTest.java
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -31,6 +31,22 @@ public class CustomerTest {
     }
 
     @Test
+    public void testRequestSavingsAccount() {
+        Bank bank = new Bank("Bank");
+        Customer customer = new Customer("Name");
+        bank.addCustomer(customer);
+        Assertions.assertTrue(customer.requestSavingsAccount());
+        Assertions.assertEquals(1, customer.getAccounts().size());
+
+        Customer customer2 = new Customer("Name");
+        Assertions.assertFalse(customer2.requestSavingsAccount());
+
+        Customer customer3 = new Customer("Name");
+        customer3.setBank(new Bank("Name"));
+        Assertions.assertFalse(customer3.requestCurrentAccount());
+    }
+
+    @Test
     public void testGetAccounts() {
         Bank bank = new Bank("Bank");
         Customer customer = new Customer("Name");

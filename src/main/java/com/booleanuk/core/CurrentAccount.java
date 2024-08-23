a2d514e28bc8e85e6f6c4f2aa78ddf35607988d6
sebgro98 <sebgro@kth.se>
2024-08-23 14:44:01 +0200

    test for currentAccount passing

--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -5,14 +5,15 @@ public class CurrentAccount extends Account{
     CurrentAccount(String accountNumber, Branch branch) {
         super(accountNumber, branch);
     }
+
     @Override
     public void deposit(int amount) {
-
+        this.addTransaction(amount, true);
     }
 
     @Override
     public void withdraw(int amount) {
-
+        this.addTransaction(amount, false);
     }
 
 }

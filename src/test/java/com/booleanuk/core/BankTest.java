e5d42bc0f9e16ef9659787391193528526e806e5
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-23 09:10:13 +0200

    Bank fail

--- src/test/java/com/booleanuk/core/BankTest.java
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -1,4 +1,17 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankTest {
+
+    Bank bank;
+
+    @Test
+    public void testBankCreate() {
+        bank = new Bank();
+        assertEquals(0, bank.getId());
+
+    }
 }

1cf205de8bba1c94fd84fcb3077d47312ec451a3
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-23 11:14:59 +0200

    Made makeAccountTest source code and passes

---
+++ src/main/java/com/booleanuk/core/Account.java
@@ -0,0 +1,18 @@
+package com.booleanuk.core;
+
+public class Account {
+    private String accountID;
+    private double balance;
+    private Customer customer;
+
+    public Account(double balance, Customer customer) {
+        this.accountID = generateRandomAccountId();
+        this.balance = balance;
+        this.customer = customer;
+    }
+
+    private String generateRandomAccountId() {
+        long randomNumber = (long) (Math.random() * 1_000_000_000_000L);
+        return String.format("%012d", randomNumber);
+    }
+}

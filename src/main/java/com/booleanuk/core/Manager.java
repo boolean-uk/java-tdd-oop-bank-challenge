7d344fa2706bc1e12f89461b3adeb87d57f9c918
Dag Andreas <daanfofo@gmail.com>
2024-08-26 13:33:51 +0200

    adding base to manager

--- src/main/java/com/booleanuk/core/Manager.java
+++ src/main/java/com/booleanuk/core/Manager.java
@@ -1,4 +1,20 @@
 package com.booleanuk.core;
 
 public class Manager {
+	private Branch branch;
+	private Bank bank;
+
+	public Manager(Bank b){
+		this.bank = b;
+		this.branch = b.getBranch();
+	}
+
+	public void concludeOverdraftRequestByIndex(boolean isAccepted, int index){
+		OverdraftRequest or = bank.getOverdraftRequest(index);
+		if(isAccepted){
+			or.accept();
+			return;
+		}
+		or.deny();
+	}
 }

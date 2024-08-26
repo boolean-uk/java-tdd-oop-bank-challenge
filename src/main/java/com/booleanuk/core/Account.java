5db4673efe712c4ffee7d9191f539be0829a55c9
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-26 09:17:15 +0200

    Domain model for bank challenge, with test of account

---
+++ src/main/java/com/booleanuk/core/Account.java
@@ -0,0 +1,10 @@
+import com.booleanuk.core.Transaction;
+
+import java.util.ArrayList;
+
+public class Account {
+    private int id;
+    private float balance;
+    private ArrayList<Transaction> transactionsList;
+
+}
---
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -0,0 +1,37 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+
+    @Test
+    public void createAccountTest() {
+        Account account = new Account(500);
+    }
+
+    @Test
+    public void getCurrentBalance() {
+        Account account = new Account(500);
+
+        Assertions.assertEquals(500, account.getBalance());
+    }
+
+    @Test
+    public void depositMoneyTest() {
+        Account account = new Account(500);
+
+        account.depositMoney(1000);
+
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void withdrawMoneyTest() {
+        Account account = new Account(500);
+
+        account.withdrawMoney(250);
+
+        Assertions.assertEquals(250, account.getBalance());
+    }
+}

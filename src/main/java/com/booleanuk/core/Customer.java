9e3b97ad28e624c000b3234fb1584c8170f9f1d4
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-26 08:04:48 +0200

    Implemented Customer clas

---
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -0,0 +1,15 @@
+package com.booleanuk.core;
+
+public class Customer {
+
+    private final String id;
+
+    public Customer(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}

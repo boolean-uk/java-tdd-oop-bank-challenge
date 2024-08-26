a45ed938ba504c9c90eb1cacf93d7034f631df7a
Stian Rusvik <stian.rusvik@no.experis.com>
2024-08-26 11:04:14 +0200

    made the transaction statement output prettier.

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,13 +1,14 @@
 package com.booleanuk.core;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Transaction {
     private LocalDate date;
     private double amount;
     private double balance;
 
-    public Transaction(LocalDate date, double amount, double balance) {
-        this.date = date;
+    public Transaction(double amount, double balance) {
+        this.date = LocalDate.now();
         this.amount = amount;
         this.balance = balance;
     }
@@ -26,6 +27,26 @@ public class Transaction {
 
     @Override
     public String toString() {
-        return "Date: " + date + "\n" + "Amount: " + amount + "\n" + "Balance: " + balance;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String credit = amount > 0 ? String.format("%.2f", amount) : "";
+        String debit = amount < 0 ? String.format("%.2f", Math.abs(amount)) : "";
+
+        String title = "\t ~~~~ TRANSACTION HISTORY ~~~~ ";
+        String columnsTitle = String.format("%-10s || %-6s || %-6s || %-6s", "date", "credit", "debit", "balance");
+        String transactionDetails = String.format("%-10s || %-6s || %-6s || %-6s",
+                date.format(formatter),
+                credit,
+                debit,
+                String.format("%.2f", balance)
+        );
+
+        return title + "\n".repeat(2) + columnsTitle + "\n" + transactionDetails;
+    }
+
+    public static void main(String[] args) {
+
+        Transaction transaction = new Transaction(100.0, 400.0);
+        System.out.println(transaction);
     }
 }

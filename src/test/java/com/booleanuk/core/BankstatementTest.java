3dd5f8622387ce48fda17bcd5540eab19227f071
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-26 14:23:52 +0200

    Test for acceptance criteria TestPrintBankstatement()

--- src/test/java/com/booleanuk/core/BankstatementTest.java
+++ src/test/java/com/booleanuk/core/BankstatementTest.java
@@ -15,6 +15,22 @@ public class BankstatementTest {
         currentAccount.withdraw(500);
 
         //Should fail due to now being correct size.
-        Assertions.assertEquals(4, currentAccount.getBankStatement().size());
+        Assertions.assertEquals(3, currentAccount.getBankStatement().size());
     }
+
+    @Test
+    public void TestPrintBankstatement(){
+        AccountOwner me = new AccountOwner("PS1234", "Benjamin Cool");
+        CurrentAccount currentAccount = new CurrentAccount(me);
+
+        currentAccount.deposit(1000);
+        currentAccount.deposit(2000);
+        currentAccount.withdraw(500);
+
+        currentAccount.printBankstatement();
+
+
+    }
+
+
 }

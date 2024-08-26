8c627a724f3d326359c8d8b6de8b3e5676253b6f
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-26 12:22:55 +0200

    red test TestIfDepositWorks(), TestIfOverDraftWorks()

---
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -0,0 +1,45 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CurrentAccountTest {
+
+    @Test
+    public void TestIfDepositWorks(){
+
+        AccountOwner me = new AccountOwner("PS1234", "Benjamin Cool");
+        CurrentAccount currentAccount  = new CurrentAccount(me);
+
+        //Should give correct return message and increase the arraylist where the transactions are stored by one.
+        Assertions.assertEquals("New Balance: 1000.0£", currentAccount.deposit(1000));
+        Assertions.assertEquals(1, currentAccount.getBankStatement().size());
+
+        Assertions.assertEquals("New Balance: 3000.0£", currentAccount.deposit(2000));
+        Assertions.assertEquals(2, currentAccount.getBankStatement().size());
+
+    }
+
+    @Test
+    public void TestIfOverDraftWorks(){
+        //OverdraftLimit is initially set at 1000£
+        AccountOwner me = new AccountOwner("PS1234", "Benjamin Cool");
+        CurrentAccount currentAccount  = new CurrentAccount(me);
+
+        Assertions.assertEquals(1000.0, currentAccount.getOverdraftLimit(), 0.0001f);
+
+        //First I try to withdraw an amount over the overdraft limit, this should give me an "Insufficient funds" message. '
+        Assertions.assertEquals("Insufficient Funds", currentAccount.withdraw(1500));
+
+
+        //I have 0 in account and withdraw 700, the overdraft kicks in.
+        currentAccount.withdraw(700);
+
+        //After using the overdraft I should now only have 300 left before the limit kicks in.
+        Assertions.assertEquals(300.0, currentAccount.getOverdraftLimit(), 0.0001f);
+
+        //If I try once more with 700 withdrawal, exceeding the limit I should get Insufficient funds message again.
+        Assertions.assertEquals("Insufficient Funds", currentAccount.withdraw(400));
+
+    }
+}

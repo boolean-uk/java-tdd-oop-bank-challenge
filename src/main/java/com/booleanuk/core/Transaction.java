c95b7321f9c5b65c4c5c05f5730a4ead0382eb6c
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-08-26 10:49:06 +0200

    Test deposit() fail

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,19 +1,20 @@
 package com.booleanuk.core;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.Locale;
 import java.util.UUID;
 
 public class Transaction {
 
     private String id;
-    private Date date;
+    private LocalDate date;
     private int credit;
     private int debit;
-    private int balance;
 
     public Transaction(int credit, int debit){
         this.id = UUID.randomUUID().toString();
-        this.date = new Date();
+        this.date = LocalDate.now();
         this.credit = credit;
         this.debit = debit;
     }
@@ -22,31 +23,15 @@ public class Transaction {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
+    public LocalDate getDate() {
         return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
     }
 
     public int getCredit() {
         return credit;
     }
 
-    public void setCredit(int credit) {
-        this.credit = credit;
-    }
-
     public int getDebit() {
         return debit;
-    }
-
-    public void setDebit(int debit) {
-        this.debit = debit;
     }
 }
--- src/test/java/com/booleanuk/core/TestCore.java
+++ src/test/java/com/booleanuk/core/TestCore.java
@@ -3,6 +3,9 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class TestCore{
 
     @Test
@@ -10,6 +13,7 @@ public class TestCore{
         Customer customer1 = new Customer("customer1");
         Assertions.assertTrue(customer1.getCurrent().getTransactions().isEmpty());
         Assertions.assertEquals(500, customer1.getCurrent().deposit(500).getCredit());
+        Assertions.assertEquals(LocalDate.now(), customer1.getCurrent().deposit(100).getDate());
         Assertions.assertEquals(1, customer1.getCurrent().getTransactions().size());
     }
 

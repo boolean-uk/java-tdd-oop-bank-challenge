9c6fe5bf217fa985365008a12b33ffba06fdfd66
Dag Andreas <daanfofo@gmail.com>
2024-08-26 09:25:28 +0200

    created transaction class

---
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+	public final double amount;
+	public final LocalDateTime date;
+
+	public Transaction(double amount){
+		this.amount = amount;
+		this.date = LocalDateTime.now();
+	}
+
+}

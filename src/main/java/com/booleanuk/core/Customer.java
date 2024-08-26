4ab8364b6e1772ca47d1529fbc5469c01c03a880
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-26 09:12:06 +0200

    Customer now holds an arraylist with their accounts

--- src/main/java/com/booleanuk/core/Customer.java
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -1,15 +1,24 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Customer {
 
     private final String id;
+    private ArrayList<Account> accounts;
 
     public Customer() {
         this.id = "";
+        this.accounts = new ArrayList<>();
     }
 
     public Customer(String id) {
         this.id = id;
+        this.accounts = new ArrayList<>();
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return this.accounts;
     }
 
     public String getId() {

f57670f5d84d2808b1c3f9eeeb938ab510ebc1d6
Dag Andreas <daanfofo@gmail.com>
2024-08-26 12:12:36 +0200

    testing generateStatement

--- src/test/java/com/booleanuk/core/TestBankAccount.java
+++ src/test/java/com/booleanuk/core/TestBankAccount.java
@@ -29,4 +29,28 @@ public class TestBankAccount {
 
 	}
 
+	@Test
+	public void testGenerateBankStatement(){
+		BankAccount ba = new BankAccount(new Customer(new Bank(Branch.Oslo), "Ole"));
+		ba.deposit(1500);
+		ba.withdraw(300);
+		ba.deposit(500);
+
+		String result = ba.generateStatement();
+		String expected = "  2024-08-26 ||      500.0 ||            ||   1700.0\n" +
+				"  2024-08-26 ||            ||      300.0 ||   1200.0\n" +
+				"  2024-08-26 ||     1500.0 ||            ||   1500.0";
+		Assertions.assertTrue(result.contains(expected));
+
+		ba.withdraw(750);
+		result = ba.generateStatement();
+		expected = "    date     ||  deposit   ||  withdraw  || balance \n" +
+				"  2024-08-26 ||            ||      750.0 ||    950.0\n" +
+				"  2024-08-26 ||      500.0 ||            ||   1700.0";
+
+		Assertions.assertTrue(result.contains(expected));
+
+
+	}
+
 }

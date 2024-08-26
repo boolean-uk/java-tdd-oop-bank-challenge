0aa7794381484eb6f846cc7cc6ffb26278d8136e
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-26 14:04:20 +0200

    adding main to just check the code and print it out

---
+++ src/main/java/com/booleanuk/core/Main.java
@@ -0,0 +1,24 @@
+package com.booleanuk.core;
+
+public class Main {
+    public static void main(String[] args) {
+        Branch osloBranch = new Branch("Oslo");
+        Branch trondheimBranch = new Branch("Trondheim");
+        Customer customer = new Customer("Melvin", "Seelan");
+        CurrentAccount currentAccount = new CurrentAccount(500, osloBranch);
+        SavingAccount savingAccount = new SavingAccount(1000, 2.5, trondheimBranch);
+        customer.addAccount(currentAccount);
+        customer.addAccount(savingAccount);
+        System.out.println("Deposit Result: " + currentAccount.deposit(100));
+        System.out.println("Withdrawal Result: " + currentAccount.withdraw(400));
+        currentAccount.requestOverdraft(300);
+        currentAccount.approveOverdraft(300);
+        System.out.println("Withdrawal Result with Overdraft: " + currentAccount.withdraw(600));
+        System.out.println("Current Account Balance: " + currentAccount.getBalance());
+        System.out.println("Saving Account Balance: " + savingAccount.getBalance());
+        System.out.println("Current Account Branch: " + currentAccount.getBranch());
+        System.out.println("Saving Account Branch: " + savingAccount.getBranch());
+        savingAccount.applyInterest();
+        savingAccount.withdraw(400);
+    }
+}

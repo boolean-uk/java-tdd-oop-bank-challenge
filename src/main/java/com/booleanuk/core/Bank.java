a43dd893fe11e9f26193584fbec5cccc9e02e95f
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-26 08:25:15 +0200

    Added bank class

---
+++ src/main/java/com/booleanuk/core/Bank.java
@@ -0,0 +1,16 @@
+package com.booleanuk.core;
+
+public class Bank {
+
+    private String name;
+
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}

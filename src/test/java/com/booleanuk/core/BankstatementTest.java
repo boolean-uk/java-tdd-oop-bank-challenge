c3fad3d9b5bc714b96a9aff784f1d5fc1f3277a6
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-26 13:06:30 +0200

    red test TestBankstatementSize()

---
+++ src/test/java/com/booleanuk/core/BankstatementTest.java
@@ -0,0 +1,20 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BankstatementTest {
+
+    @Test
+    public void TestBankstatementSize(){
+        AccountOwner me = new AccountOwner("PS1234", "Benjamin Cool");
+        CurrentAccount currentAccount = new CurrentAccount(me);
+        
+        currentAccount.deposit(1000);
+        currentAccount.deposit(2000);
+        currentAccount.withdraw(500);
+
+        //Should fail due to now being correct size.
+        Assertions.assertEquals(4, currentAccount.getBankStatement().size());
+    }
+}

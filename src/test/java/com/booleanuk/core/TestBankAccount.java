fc6f4e005d81e328ca351177c37b6622a4a47905
Dag Andreas <daanfofo@gmail.com>
2024-08-26 11:29:19 +0200

    testing deposit and withdraw

--- src/test/java/com/booleanuk/core/TestBankAccount.java
+++ src/test/java/com/booleanuk/core/TestBankAccount.java
@@ -1,5 +1,6 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestBankAccount {
@@ -7,6 +8,25 @@ public class TestBankAccount {
 	@Test
 	public void testBankAccountConstr(){
 		BankAccount ba = new BankAccount(new Customer(new Bank(Branch.Oslo), "Ole"));
+	}
+
+
+	@Test
+	public void testDepositAndWithDraw(){
+		BankAccount ba = new BankAccount(new Customer(new Bank(Branch.Oslo), "Ole"));
+		double balance;
+		balance = ba.getBalance();
+		Assertions.assertEquals(0, balance);
+
+		ba.deposit(15);
+		balance = ba.getBalance();
+		Assertions.assertEquals(15, balance);
+
+
+		ba.withdraw(3);
+		balance = ba.getBalance();
+		Assertions.assertEquals(12, balance);
+
 	}
 
 }

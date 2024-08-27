9b3723936b10930a0e9bbc21b7105d8f143cb6e4
gremble0 <hermanstornes@gmail.com>
2024-08-27 08:46:11 +0200

    Dont allow overdrafts on savings accounts

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,23 +1,83 @@
 package com.booleanuk.core;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface Account {
-  int balance();
+public class Account {
+  protected List<Transaction> transactions;
+  protected Branch branch;
 
-  void deposit(int amount);
+  public Account(Branch branch) {
+    this.transactions = new ArrayList<>();
+    this.branch = branch;
+  }
 
-  void deposit(int amount, LocalDateTime time);
+  public Account(Branch branch, int deposit) {
+    this(branch);
+    this.transactions.add(new Transaction(deposit, TransactionType.DEPOSIT, LocalDateTime.now()));
+  }
 
-  void withdraw(int amount) throws OverdraftException;
+  public Branch branch() {
+    return this.branch;
+  }
 
-  void withdraw(int amount, LocalDateTime time) throws OverdraftException;
+  public int balance() {
+    return this.balance(this.transactions.size() - 1);
+  }
 
-  void forceWithdraw(int amount);
+  private int balance(int nTransactions) {
+    int balance = 0;
+    for (int i = 0; i <= nTransactions; ++i) {
+      Transaction transaction = this.transactions.get(i);
+      if (transaction.type() == TransactionType.DEPOSIT)
+        balance += transaction.amount();
+      else if (transaction.type() == TransactionType.WITHDRAWAL)
+        balance -= transaction.amount();
+    }
 
-  void forceWithdraw(int amount, LocalDateTime time);
+    return balance;
+  }
 
-  String getHistory();
+  public void deposit(int amount) {
+    this.deposit(amount, LocalDateTime.now());
+  }
 
-  Branch branch();
+  public void deposit(int amount, LocalDateTime time) {
+    this.transactions.add(new Transaction(amount, TransactionType.DEPOSIT, time));
+  }
+
+  public void withdraw(int amount) throws OverdraftException {
+    this.withdraw(amount, LocalDateTime.now());
+  }
+
+  public void withdraw(int amount, LocalDateTime time) throws OverdraftException {
+    int balance = this.balance();
+    if (balance - amount < 0)
+      throw new OverdraftException(balance, amount, this);
+
+    this.transactions.add(new Transaction(amount, TransactionType.WITHDRAWAL, time));
+  }
+
+  public void forceWithdraw(int amount) {
+    this.forceWithdraw(amount, LocalDateTime.now());
+  }
+
+  public void forceWithdraw(int amount, LocalDateTime time) {
+    System.out.println("alksdlkalskd " + this.balance());
+    this.transactions.add(new Transaction(amount, TransactionType.WITHDRAWAL, time));
+    System.out.println("alksdlkalskd2222 " + this.balance());
+  }
+
+  public String getHistory() {
+    StringBuilder history = new StringBuilder();
+    history.append(String.format("|| %-20s|| %-10s|| %-10s|| %-10s||\n", "date", "credit", "debit", "balance"));
+
+    for (int i = this.transactions.size() - 1; i >= 0; --i) {
+      Transaction transaction = this.transactions.get(i);
+      history.append(transaction.toStringWithBalance(this.balance(i)));
+    }
+
+    return history.toString();
+  }
 }
--- src/main/java/com/booleanuk/core/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/CurrentAccount.java
@@ -1,92 +1,11 @@
 package com.booleanuk.core;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-public class CurrentAccount implements Account {
-  private List<Transaction> transactions;
-  private Branch branch;
-
+public class CurrentAccount extends Account {
   public CurrentAccount(Branch branch) {
-    this.transactions = new ArrayList<>();
-    this.branch = branch;
+    super(branch);
   }
 
   public CurrentAccount(Branch branch, int deposit) {
-    this(branch);
-    this.transactions.add(new Transaction(deposit, TransactionType.DEPOSIT, LocalDateTime.now()));
-  }
-
-  @Override
-  public Branch branch() {
-    return this.branch;
-  }
-
-  @Override
-  public int balance() {
-    return this.balance(this.transactions.size() - 1);
-  }
-
-  private int balance(int nTransactions) {
-    int balance = 0;
-    for (int i = 0; i <= nTransactions; ++i) {
-      Transaction transaction = this.transactions.get(i);
-      if (transaction.type() == TransactionType.DEPOSIT)
-        balance += transaction.amount();
-      else if (transaction.type() == TransactionType.WITHDRAWAL)
-        balance -= transaction.amount();
-    }
-
-    return balance;
-  }
-
-  @Override
-  public void deposit(int amount) {
-    this.deposit(amount, LocalDateTime.now());
-  }
-
-  @Override
-  public void deposit(int amount, LocalDateTime time) {
-    this.transactions.add(new Transaction(amount, TransactionType.DEPOSIT, time));
-  }
-
-  @Override
-  public void withdraw(int amount) throws OverdraftException {
-    this.withdraw(amount, LocalDateTime.now());
-  }
-
-  @Override
-  public void withdraw(int amount, LocalDateTime time) throws OverdraftException {
-    int balance = this.balance();
-    if (balance - amount < 0)
-      throw new OverdraftException(balance, amount, this);
-
-    this.transactions.add(new Transaction(amount, TransactionType.WITHDRAWAL, time));
-  }
-
-  @Override
-  public void forceWithdraw(int amount) {
-    this.forceWithdraw(amount, LocalDateTime.now());
-  }
-
-  @Override
-  public void forceWithdraw(int amount, LocalDateTime time) {
-    System.out.println("alksdlkalskd " + this.balance());
-    this.transactions.add(new Transaction(amount, TransactionType.WITHDRAWAL, time));
-    System.out.println("alksdlkalskd2222 " + this.balance());
-  }
-
-  @Override
-  public String getHistory() {
-    StringBuilder history = new StringBuilder();
-    history.append(String.format("|| %-20s|| %-10s|| %-10s|| %-10s||\n", "date", "credit", "debit", "balance"));
-
-    for (int i = this.transactions.size() - 1; i >= 0; --i) {
-      Transaction transaction = this.transactions.get(i);
-      history.append(transaction.toStringWithBalance(this.balance(i)));
-    }
-
-    return history.toString();
+    super(branch, deposit);
   }
 }

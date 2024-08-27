29cec9ed107300c70194493f6c3817aca79b36aa
TMajlu <thomasmajnie@gmail.com>
2024-08-27 13:24:45 +0200

    Completed most core exercises and extensions.

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.lang.Math;
 
@@ -95,12 +96,6 @@ public class AccountTest {
         Assertions.assertFalse(currentAccount.withdraw(1001, "27/07/2024"));
 
     }
-
-
-
-
-
-
 
 
 

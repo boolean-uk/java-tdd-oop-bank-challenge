8aa05d10c635ab4e605732212fdf702b1cd10ff2
gremble0 <hermanstornes@gmail.com>
2024-08-27 09:04:25 +0200

    Test that savings accounts cannot overdraft

--- src/test/java/com/booleanuk/core/SavingsAccountTest.java
+++ src/test/java/com/booleanuk/core/SavingsAccountTest.java
@@ -1,39 +1,15 @@
 package com.booleanuk.core;
 
+import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SavingsAccountTest {
   @Test
-  public void testWithdraw() {
-    Account account = new SavingsAccount(Branch.OSLO, 100);
-
-    account.withdraw(50);
-    Assertions.assertEquals(50, account.balance());
-
-    account.withdraw(20);
-    Assertions.assertEquals(30, account.balance());
-  }
-
-  @Test
-  public void testDeposit() {
-    Account account = new SavingsAccount(Branch.OSLO, 100);
-
-    account.deposit(50);
-    Assertions.assertEquals(150, account.balance());
-
-    account.deposit(20);
-    Assertions.assertEquals(170, account.balance());
-  }
-
-  @Test
-  public void testWithdrawDeposit() {
-    Account account = new SavingsAccount(Branch.OSLO, 100);
-
-    account.withdraw(50);
-    Assertions.assertEquals(50, account.balance());
-
-    account.deposit(50);
-    Assertions.assertEquals(100, account.balance());
+  public void testCannotOverdraft() {
+    Account account = new SavingsAccount(Branch.OSLO);
+    account.deposit(200);
+    Assertions.assertDoesNotThrow(() -> account.withdraw(200));
+    Assertions.assertThrows(CantOverdraftException.class, () -> account.withdraw(200));
   }
 }

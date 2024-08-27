57d3787770fb06aaad65618e5b094fdc16ddc513
Iris Flystam <badde00@gmail.com>
2024-08-27 09:42:45 +0200

    GREEN: testUserCreation passes

--- src/main/java/com/booleanuk/core/User.java
+++ src/main/java/com/booleanuk/core/User.java
@@ -5,4 +5,13 @@ import java.util.ArrayList;
 public class User {
     private ArrayList<Account> accounts;
     private String userId;
+
+    public User(String userId) {
+        this.accounts = new ArrayList<>();
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }

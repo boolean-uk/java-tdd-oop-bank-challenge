114e247248a393cbf3cf2228013ecb5a2b0dd11c
Dag Andreas <daanfofo@gmail.com>
2024-08-27 10:20:47 +0200

    bordervalue analysis unit test on withdraw func to test overdraft value

--- src/test/java/com/booleanuk/core/TestBankAccountDebit.java
+++ src/test/java/com/booleanuk/core/TestBankAccountDebit.java
@@ -49,8 +49,80 @@ public class TestBankAccountDebit {
 				"  2024-08-26 ||      500.0 ||            ||   1700.0";
 
 		Assertions.assertTrue(result.contains(expected));
+	}
+
+	@Test
+	public void testWithdrawBelowZero(){
+		Bank b = new Bank(Branch.Oslo);
+		Customer c = b.createCustomer("Kåre Kåre Kåre");
+		BankAccount ba = c.createAccount(BankType.Debit);
+		String ans  = ba.withdraw(1000);
+		String expected = "Couldn't withdraw 1000.0 from account.";
+		Assertions.assertEquals(expected, ans);
+
+		boolean sentRequest = ba.requestOverdraft();
+		Assertions.assertTrue(sentRequest);
+
+		OverdraftRequest or = b.getOverdraftRequest(0);
+		or.accept();
+
+		ans  = ba.withdraw(1000);
+		expected = "withdrew " + 1000.0 +  " from account.";
+		Assertions.assertEquals(expected, ans);
+	}
+
+	@Test
+	public void testOverdraft(){
+		Bank b = new Bank(Branch.Oslo);
+		Customer c = b.createCustomer("Kåre Kåre Kåre");
+		BankAccount ba = c.createAccount(BankType.Debit);
+		ba.setCanOverdraft(true);
+		ba.deposit(20000);
+
+
+
+		String result = ba.withdraw(35000);
+		String expected = "Couldn't withdraw " + 35000d + " from account.";
+		Assertions.assertEquals(expected, result);
+
+		result = ba.withdraw(25000);
+		expected = "withdrew " + 25000d + " from account.";
+		Assertions.assertEquals(expected, result);
+
+		// bva testing
+		BankAccount ba2 = c.createAccount(BankType.Debit);
+		ba2.setCanOverdraft(true);
+		result = ba2.withdraw(10000);
+		expected = "withdrew " + 10000d + " from account.";
+		Assertions.assertEquals(expected, result);
+
+		BankAccount ba3 = c.createAccount(BankType.Debit);
+		ba3.setCanOverdraft(true);
+		result = ba3.withdraw(10000.0001);
+		expected = "Couldn't withdraw " + 10000.0001d + " from account.";
+		Assertions.assertEquals(expected, result);
 
 
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

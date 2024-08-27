cd721d177cb107d0695d5a2e5f3dae1d6f6bb6d5
Iris Flystam <badde00@gmail.com>
2024-08-27 15:42:43 +0200

    RED: testGetAccount

--- src/test/java/com/booleanuk/core/UserTest.java
+++ src/test/java/com/booleanuk/core/UserTest.java
@@ -8,32 +8,39 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
-     @Test
-     public void testUserCreation() {
-         User user = new User("0001", "John");
-         Assertions.assertEquals("0001", user.getUserId());
-         Assertions.assertEquals("John", user.getName());
-     }
+    @Test
+    public void testUserCreation() {
+        User user = new User("0001", "John");
+        Assertions.assertEquals("0001", user.getUserId());
+        Assertions.assertEquals("John", user.getName());
+    }
 
-     @Test
-     public void testGetAccountNumbers() {
-         User user = new User("1", "John");
-         user.addAccount(new SavingsAccount("1", "0000", "001"));
-         user.addAccount(new CurrentAccount("1", "0000", "002"));
-         ArrayList<String> accNums = user.getAccountNumbers();
-         Assertions.assertEquals("001", accNums.getFirst());
-         Assertions.assertEquals("002", accNums.getLast());
-     }
+    @Test
+    public void testGetAccountNumbers() {
+        User user = new User("1", "John");
+        user.addAccount(new SavingsAccount("1", "0000", "001"));
+        user.addAccount(new CurrentAccount("1", "0000", "002"));
+        ArrayList<String> accNums = user.getAccountNumbers();
+        Assertions.assertEquals("001", accNums.getFirst());
+        Assertions.assertEquals("002", accNums.getLast());
+    }
 
-     @Test
+    @Test
     public void testAddAccounts() {
-         User user = new User("1", "John");
-         ArrayList<String> accNums = user.getAccountNumbers();
-         Assertions.assertTrue(accNums.isEmpty());
-         user.addAccount(new SavingsAccount("1", "0000", "001"));
-         accNums = user.getAccountNumbers();
-         Assertions.assertEquals("001", accNums.getFirst());
-         Assertions.assertEquals(1, user.getAccountNumbers().size());
-     }
+        User user = new User("1", "John");
+        ArrayList<String> accNums = user.getAccountNumbers();
+        Assertions.assertTrue(accNums.isEmpty());
+        user.addAccount(new SavingsAccount("1", "0000", "001"));
+        accNums = user.getAccountNumbers();
+        Assertions.assertEquals("001", accNums.getFirst());
+        Assertions.assertEquals(1, user.getAccountNumbers().size());
+    }
 
+    @Test
+    public void testGetAccount() {
+        User user = new User("1", "John");
+        Account a = new SavingsAccount("1", "0000", "001");
+        user.addAccount(a);
+        Assertions.assertEquals(a, user.getAccount("001"));
+    }
 }

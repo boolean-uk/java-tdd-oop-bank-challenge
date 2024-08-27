a5cdaa49077bbb70b448597af756ea8c9ed4d0d8
Connor Stokes <connorjordanstokes@gmail.com>
2024-08-27 13:38:44 +0100

    testgetBalance() built and fails

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,5 +1,6 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
@@ -8,6 +9,14 @@ public class AccountTest {
         Account account = new Account(15.00);
         account.depositFunds(5.00);
         System.out.println(account.transactions.get(0).getTransactionAmount());
+        Assertions.assertEquals(5,account.transactions.get(0).getTransactionAmount());
+    }
 
+    @Test
+    public void testgetBalance(){
+        Account account = new Account(15.00);
+        account.depositFunds(5.00);
+        System.out.println(account.getBalance());
+        Assertions.assertEquals(5,account.getBalance());
     }
 }

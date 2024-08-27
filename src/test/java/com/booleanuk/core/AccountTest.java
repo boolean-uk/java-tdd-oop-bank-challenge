b93ddd53ebf097df846c635e07f2ea9f1a8b15ba
Connor Stokes <connorjordanstokes@gmail.com>
2024-08-27 13:42:25 +0100

    testWithdrawFunds() built and fails

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -19,4 +19,12 @@ public class AccountTest {
         System.out.println(account.getBalance());
         Assertions.assertEquals(5,account.getBalance());
     }
+
+    @Test
+    public void testWithdrawFunds(){
+        Account account = new Account(15.00);
+        account.depositFunds(5.00);
+        System.out.println(account.transactions.get(0).getTransactionAmount());
+        Assertions.assertEquals(5,account.transactions.get(0).getTransactionAmount());
+    }
 }

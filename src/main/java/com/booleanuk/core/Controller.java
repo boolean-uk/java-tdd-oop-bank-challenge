1b08250d430c830849fb2d1f3ad9fe50e6dcc3d2
Iris Flystam <badde00@gmail.com>
2024-08-27 10:34:32 +0200

    REFACTOR: ui to add users

--- src/main/java/com/booleanuk/core/Controller.java
+++ src/main/java/com/booleanuk/core/Controller.java
@@ -2,6 +2,7 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Controller {
     public static HashMap<String, Depositable> depositables = new HashMap<>();
@@ -17,10 +18,52 @@ public class Controller {
 
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.start();
+        controller.mainMenu();
     }
 
-    private void start() {
-        System.out.println("TODO");
+    private void mainMenu() {
+        boolean keepRunning = true;
+        while (keepRunning) {
+            int chosenOption = view.mainMenu();
+            switch (chosenOption) {
+                case 1:
+                    String userId = generateUserId();
+                    users.add(view.getUser(userId));
+                    view.printUser(users.getLast());
+                    break;
+                case 12:
+                    users.add(new User("11", "Huey"));
+                    users.add(new User("12", "Dewey"));
+                    users.add(new User("21", "Louie"));
+                    Account a1 = new CurrentAccount(users.getFirst().getUserId(), "0000", "000");
+                    Account a2 = new CurrentAccount(users.get(1).getUserId(), "0000", "001");
+                    Account a3 = new SavingsAccount(users.get(1).getUserId(), "0000", "002");
+                    Account a4 = new CurrentAccount(users.getLast().getUserId(), "0001", "003");
+                    Account a5 = new CurrentAccount(users.getLast().getUserId(), "0001", "004");
+                    Account a6 = new CurrentAccount(users.getLast().getUserId(), "0000", "005");
+                    users.getFirst().addAccount(a1);
+                    users.get(1).addAccount(a2);
+                    users.get(1).addAccount(a3);
+                    users.getLast().addAccount(a4);
+                    users.getLast().addAccount(a5);
+                    users.getLast().addAccount(a6);
+                    System.out.println("Accounts added");
+                    break;
+                default:
+                    keepRunning = false;
+            }
+        }
+    }
+
+    private String generateUserId() {
+        Random random = new Random();
+        long randomNumber = random.nextLong() % 10000000000L;
+        randomNumber = Math.abs(randomNumber);
+        String stringNumber = String.format("%010d", randomNumber);
+        String finalStringNumber = stringNumber;
+        if (users.stream().anyMatch(user -> user.getUserId().equals(finalStringNumber))) {
+            stringNumber = generateUserId();
+        }
+        return stringNumber;
     }
 }
--- src/main/java/com/booleanuk/core/View.java
+++ src/main/java/com/booleanuk/core/View.java
@@ -8,4 +8,38 @@ public class View {
     public View() {
         scanner = new Scanner(System.in);
     }
+
+    public int mainMenu() {
+        System.out.println("What do you want to do?");
+        System.out.println("1. Add User");
+        System.out.println("12. (For testing) Add starting data");
+
+        return getInt();
+    }
+
+    public int getInt() {
+        int output;
+        try {
+            output = scanner.nextInt();
+        } catch (Exception e) {
+            System.err.println("Something went wrong");
+            output = Integer.MIN_VALUE;
+        }
+        return output;
+    }
+
+    public User getUser(String userId) {
+        System.out.println("Enter user name");
+        String name = scanner.nextLine();
+        return new User(userId, name);
+    }
+
+    public void printUser(User user) {
+        System.out.println("User ID: " + user.getUserId());
+        System.out.println("User name: " + user.getName());
+        System.out.println("Account numbers: ");
+        for (String str: user.getAccountNumbers()) {
+            System.out.println("  " + str);
+        }
+    }
 }

30a8bd23f79d518fff904afdfac8663d8b130c3d
Iris Flystam <badde00@gmail.com>
2024-08-27 17:24:37 +0200

    RED: testWithdraw, testWithdrawTooMuch

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -20,8 +20,16 @@ class CurrentAccountTest {
         CurrentAccount a = new CurrentAccount("1", "0000", "01");
         a.deposit(10000);
         Assertions.assertEquals(10000, a.calculateBalance());
-        Assertions.assertEquals("The 1.00 money is now deposited to the account 01.", a.withdraw(5000));
+        Assertions.assertEquals(String.format("50.00 money has been withdrawn from your account.%n"), a.withdraw(5000));
         Assertions.assertEquals(5000, a.calculateBalance());
+    }
+
+    @Test
+    public void testWithdrawTooMuch() {
+        CurrentAccount a = new CurrentAccount("1", "0000", "01");
+        Assertions.assertEquals(0, a.calculateBalance());
+        Assertions.assertEquals(String.format("Your balance of 0.00 is too low to withdraw a sum of 50.00.%n"), a.withdraw(5000));
+        Assertions.assertEquals(0, a.calculateBalance());
     }
 
 }

6b7a43611f73f4a3b59b4db438a476841c720c4d
Iris Flystam <badde00@gmail.com>
2024-08-27 17:06:43 +0200

    RED: testWithdraw

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -15,4 +15,13 @@ class CurrentAccountTest {
         Assertions.assertEquals(100, a.calculateBalance());
     }
 
+    @Test
+    public void testWithdraw() {
+        CurrentAccount a = new CurrentAccount("1", "0000", "01");
+        a.deposit(10000);
+        Assertions.assertEquals(10000, a.calculateBalance());
+        Assertions.assertEquals("The 1.00 money is now deposited to the account 01.", a.withdraw(5000));
+        Assertions.assertEquals(5000, a.calculateBalance());
+    }
+
 }

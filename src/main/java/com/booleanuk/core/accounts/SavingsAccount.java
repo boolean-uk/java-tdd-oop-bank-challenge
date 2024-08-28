7a0d6f300fcd499b542e07f8dfb47696154249fc
shyye <emma.art.dev@gmail.com>
2024-08-28 06:55:28 +0200

    Add interestRate variable to SavingsAccount

--- src/main/java/com/booleanuk/core/accounts/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/accounts/SavingsAccount.java
@@ -2,8 +2,11 @@ package com.booleanuk.core.accounts;
 
 public class SavingsAccount extends Account{
 
+    private Double interestRate;
+
     public SavingsAccount() {
         this.setBranch(Branch.BRANCH_1);
         this.setPossibleToOverdraft(false);
+        this.interestRate = 0.12;
     }
 }

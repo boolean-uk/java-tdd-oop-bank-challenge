15ffc41ce42f0ef8978c5185639979a429383a86
shyye <emma.art.dev@gmail.com>
2024-08-28 06:46:59 +0200

    Add Test and code for withdraw() and deposit()

--- src/main/java/com/booleanuk/core/accounts/Account.java
+++ src/main/java/com/booleanuk/core/accounts/Account.java
@@ -2,15 +2,22 @@ package com.booleanuk.core.accounts;
 
 import com.booleanuk.core.idgenerator.IdGenerator;
 import com.booleanuk.core.idgenerator.IdPrefix;
+import com.booleanuk.core.transactons.Transaction;
+import com.booleanuk.core.transactons.TransactionType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Account {
 
     private final String accountNumber;
     private Branch branch;
-    boolean isPossibleToOverdraft;
+    private boolean isPossibleToOverdraft;
+    private Map<String, Transaction> transactions;
 
     public Account() {
         this.accountNumber = new IdGenerator(IdPrefix.AC).getId();
+        this.transactions = new HashMap<>();
     }
 
     public String getAccountNumber() {
@@ -31,5 +38,60 @@ public abstract class Account {
 
     public void setPossibleToOverdraft(boolean possibleToOverdraft) {
         isPossibleToOverdraft = possibleToOverdraft;
+    }
+
+    protected void addTransaction(Transaction transaction) {
+        this.transactions.put(transaction.getId(), transaction);
+    }
+
+    public void deposit(Double amount) {
+        Transaction transaction = new Transaction(
+                amount,
+                TransactionType.DEBIT,
+                calculateBalance()
+        );
+        this.addTransaction(transaction);
+    }
+
+    protected void validateOverdraft(Double currentBalance, Double withdrawAmount) throws Exception {
+        boolean isNegativeBalance = (currentBalance - withdrawAmount) < 0;
+        if (!this.isPossibleToOverdraft && isNegativeBalance) {
+
+            // TODO: should make custom exception
+
+            throw new Exception("Not possible to overdraft");
+        }
+    }
+
+    public void withdraw(Double amount) {
+        Double currentBalance = this.calculateBalance();
+        try {
+            this.validateOverdraft(currentBalance, amount);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Transaction transaction = new Transaction(
+                amount,
+                TransactionType.CREDIT,
+                currentBalance
+        );
+        this.addTransaction(transaction);
+    }
+
+    protected Double calculateBalance() {
+        Double sum = 0.0;
+        for (Transaction transaction : transactions.values()) {
+            if (transaction.getType() == TransactionType.DEBIT) {
+                sum += transaction.getAmount();
+            } else if (transaction.getType() == TransactionType.CREDIT) {
+                sum -= transaction.getAmount();
+            }
+        }
+        return sum;
+    }
+
+    public Double getBalance() {
+        return calculateBalance();
     }
 }
--- src/test/java/com/booleanuk/core/accounts/AccountTest.java
+++ src/test/java/com/booleanuk/core/accounts/AccountTest.java
@@ -28,4 +28,33 @@ public class AccountTest {
         savingsAccount.setPossibleToOverdraft(true);
         Assertions.assertTrue(savingsAccount.isPossibleToOverdraft());
     }
+
+    @Test
+    public void depositMoney() {
+        Account currentAccount = new CurrentAccount();
+        currentAccount.deposit(1000.00);
+        Assertions.assertEquals(1000.00, currentAccount.getBalance());
+
+        Account savingtAccount = new SavingsAccount();
+        savingtAccount.deposit(1000.00);
+        Assertions.assertEquals(1000.00, savingtAccount.getBalance());
+    }
+
+    @Test
+    public void withdrawMoney() {
+        Account currentAccount = new CurrentAccount();
+        currentAccount.withdraw(1000.00);
+        Assertions.assertEquals(-1000.00, currentAccount.getBalance());
+
+        Account savingtAccount = new SavingsAccount();
+        // Inner validate function should throw error
+        Exception e = Assertions.assertThrows(
+                Exception.class,
+                () -> { savingtAccount.validateOverdraft(savingtAccount.getBalance(), 1000.00); }
+        );
+        Assertions.assertEquals("Not possible to overdraft", e.getMessage());
+
+        // Outer function .withdraw() should handle error
+        Assertions.assertDoesNotThrow(() -> savingtAccount.withdraw(1000.0));
+    }
 }

a6cca4a6c6313e7a475531dc03b76bdb923a5491
shyye <emma.art.dev@gmail.com>
2024-08-28 14:27:41 +0200

    Redo Test and Code for Accounts

--- src/main/java/com/booleanuk/core/accounts/CurrentAccount.java
+++ src/main/java/com/booleanuk/core/accounts/CurrentAccount.java
@@ -2,7 +2,11 @@ package com.booleanuk.core.accounts;
 
 public class CurrentAccount extends Account{
 
-    public CurrentAccount() {
+    // TODO: Could this constructor be protected?
+    //  should not be able to create without a Bank
+    
+    public CurrentAccount(String accountNumber) {
+        super(accountNumber);
         this.setBranch(Branch.BRANCH_1);
         this.setPossibleToOverdraft(true);
     }
--- src/main/java/com/booleanuk/core/accounts/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/accounts/SavingsAccount.java
@@ -4,7 +4,11 @@ public class SavingsAccount extends Account{
 
     private Double interestRate;
 
-    public SavingsAccount() {
+    // TODO: Could this constructor be protected?
+    //  should not be able to create without a Bank
+
+    public SavingsAccount(String accountNumber) {
+        super(accountNumber);
         this.setBranch(Branch.BRANCH_1);
         this.setPossibleToOverdraft(false);
         this.interestRate = 0.12;

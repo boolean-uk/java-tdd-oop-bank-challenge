69125d3f4d7646e4dcba3be5ade0b5eb2200bf80
shyye <emma.art.dev@gmail.com>
2024-08-28 14:16:33 +0200

    Redo Test and Code for Transactions/Accounts

--- src/test/java/com/booleanuk/core/accounts/AccountTest.java
+++ src/test/java/com/booleanuk/core/accounts/AccountTest.java
@@ -7,23 +7,23 @@ public class AccountTest {
 
     @Test
     public void createCurrentAccount() {
-        Account currentAccount = new CurrentAccount();
-        Assertions.assertEquals("AC_1", currentAccount.getAccountNumber());
+        Account currentAccount = new CurrentAccount("test_1");
+        Assertions.assertEquals("test_1", currentAccount.getAccountNumber());
         Assertions.assertEquals(Branch.BRANCH_1, currentAccount.getBranch());
 
-        Account savingsAccount = new CurrentAccount();
-        Assertions.assertEquals("AC_2", savingsAccount.getAccountNumber());
+        Account savingsAccount = new CurrentAccount("test_2");
+        Assertions.assertEquals("test_2", savingsAccount.getAccountNumber());
         Assertions.assertEquals(Branch.BRANCH_1, savingsAccount.getBranch());
     }
 
     @Test
     public void checkOverdraftFacility() {
-        Account currentAccount = new CurrentAccount();
+        Account currentAccount = new CurrentAccount("test_1");
         Assertions.assertTrue(currentAccount.isPossibleToOverdraft());
         currentAccount.setPossibleToOverdraft(false);
         Assertions.assertFalse(currentAccount.isPossibleToOverdraft());
 
-        Account savingsAccount = new SavingsAccount();
+        Account savingsAccount = new SavingsAccount("test_2");
         Assertions.assertFalse(savingsAccount.isPossibleToOverdraft());
         savingsAccount.setPossibleToOverdraft(true);
         Assertions.assertTrue(savingsAccount.isPossibleToOverdraft());
@@ -31,22 +31,22 @@ public class AccountTest {
 
     @Test
     public void depositMoney() {
-        Account currentAccount = new CurrentAccount();
+        Account currentAccount = new CurrentAccount("test_1");
         currentAccount.deposit(1000.00);
         Assertions.assertEquals(1000.00, currentAccount.getBalance());
 
-        Account savingtAccount = new SavingsAccount();
+        Account savingtAccount = new SavingsAccount("test_2");
         savingtAccount.deposit(1000.00);
         Assertions.assertEquals(1000.00, savingtAccount.getBalance());
     }
 
     @Test
     public void withdrawMoney() {
-        Account currentAccount = new CurrentAccount();
+        Account currentAccount = new CurrentAccount("test_1");
         currentAccount.withdraw(1000.00);
         Assertions.assertEquals(-1000.00, currentAccount.getBalance());
 
-        Account savingtAccount = new SavingsAccount();
+        Account savingtAccount = new SavingsAccount("test_2");
         // Inner validate function should throw error
         Exception e = Assertions.assertThrows(
                 Exception.class,

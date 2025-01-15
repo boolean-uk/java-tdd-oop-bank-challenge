aa42c32e167aadb82db5b089373afc9a244bf66e
Mattias Hedbom <mattias.hedbom@se.experis.com>
2025-01-15 10:48:18 +0100

    Created the Transaction-class and populated the ArrayList<Transaction> listOfTransactions in the BankAccountTest-class

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Transaction {
+    private LocalDateTime date;
+    private Double amount;
+    private String typeOfTransaction;
+
+    public Transaction(LocalDateTime date, Double amount, String typeOfTransaction){
+        this.date = date;
+        this.amount = amount;
+        this.typeOfTransaction = typeOfTransaction;
+    }
 }
--- src/test/java/com/booleanuk/core/BankAccountTest.java
+++ src/test/java/com/booleanuk/core/BankAccountTest.java
@@ -1,4 +1,22 @@
 package com.booleanuk.core;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class BankAccountTest {
+    private ArrayList<Transaction> listOfTransactions = new ArrayList<>(){{
+        LocalDateTime dateTime1 = LocalDateTime.now();
+        Transaction transaction1 = new Transaction(dateTime1, 200D, "Deposit");
+        add(transaction1);
+
+        LocalDateTime dateTime2 = dateTime1.plusHours(1);
+        Transaction transaction2 = new Transaction(dateTime2, 700D, "Deposit");
+        add(transaction2);
+
+        LocalDateTime dateTime3 = dateTime1.plusDays(2);
+        Transaction transaction3 = new Transaction(dateTime3, 900D, "Withdraw");
+        add(transaction3);
+    }};
+
+
 }

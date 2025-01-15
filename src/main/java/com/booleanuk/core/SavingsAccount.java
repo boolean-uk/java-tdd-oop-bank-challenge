53d2a02c66bf2f260d2c8ba95b47e1241660f078
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-15 14:51:13 +0100

    Add dummy methods for overdrafts in SavingsAccount

--- src/main/java/com/booleanuk/core/SavingsAccount.java
+++ src/main/java/com/booleanuk/core/SavingsAccount.java
@@ -1,7 +1,22 @@
 package com.booleanuk.core;
 
 public class SavingsAccount extends BankAccount {
+    private double maxOverdraft;
+
     public SavingsAccount(int branchNumber) {
         super(branchNumber);
+        this.maxOverdraft = 0;
+    }
+
+    public boolean hasOverdraft() {
+        return maxOverdraft > 0;
+    }
+
+    public double getMaxOverdraft() {
+        return maxOverdraft;
+    }
+
+    public boolean setMaxOverdraft(double maxOverdraft) {
+        return true;
     }
 }

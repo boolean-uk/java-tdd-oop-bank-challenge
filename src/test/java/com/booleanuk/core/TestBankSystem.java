b4d2b3b7e19d4c70a9844224eb7e64e195876084
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-15 15:08:18 +0100

    Add dummy test for BankSystem

---
+++ src/test/java/com/booleanuk/core/TestBankSystem.java
@@ -0,0 +1,21 @@
+package com.booleanuk.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestBankSystem {
+    @Test
+    public void cannotGoBeyondOverdraftLimit() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void canHaveNegativeBalanceWithinOverdraft() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void approvingOverdraftRequestWorksCorrectly() {
+        assertTrue(true);
+    }
+}

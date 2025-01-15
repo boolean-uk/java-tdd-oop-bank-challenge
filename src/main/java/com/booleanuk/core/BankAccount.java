c5939d02da65d88db170e55946927c2d1bdfec53
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-15 10:37:17 +0100

    Populate BankAccount with dummy methods and implement constructor

--- src/main/java/com/booleanuk/core/BankAccount.java
+++ src/main/java/com/booleanuk/core/BankAccount.java
@@ -1,4 +1,42 @@
 package com.booleanuk.core;
 
-public class BankAccount {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public abstract class BankAccount {
+    private final int accountNumber;
+    private final double balance;
+    private final List<Transaction> transactions;
+
+    public BankAccount() {
+        Random random = new Random();
+        this.accountNumber = random.nextInt(10000000, 99999999);
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public boolean withdraw(double amount) {
+        return true;
+    }
+
+    public boolean deposit(double amount) {
+        return true;
+    }
+
+    public String generateBankStatement() {
+        return "";
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
 }

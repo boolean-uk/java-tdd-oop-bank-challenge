e948102144e7b9d366fd86e8419b88d4b5b412dd
Frida Anselin <frida.anselin@se.experis.com>
2025-01-15 11:11:37 +0100

    Add getBalance test - red

---
+++ src/test/java/com/booleanuk/core/BankAccountTest.java
@@ -0,0 +1,25 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+public class BankAccountTest {
+
+    @BeforeEach
+    public void setUp() {
+        HashMap<Integer, Transaction> transactions = new HashMap<>() {{
+            put(12345, new Transaction(12345, "credit", 200.0, "2025-01-15", 200.0));
+            put(54321, new Transaction(54321, "debit", 100.0, "2025-01-15", 100.0));
+            put(32145, new Transaction(32145, "credit", 50.0, "2025-01-16", 150.0));
+        }};
+        Bankaccount bankaccount = new Bankaccount(12345678, transactions, 0.0);
+    }
+
+    @Test
+    public void testGetBalance() {
+        Assertions.assertEquals(150.0, bankaccount.getBalance(12345678));
+    }
+}

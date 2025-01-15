99465ff86fdb529ff0a0b0130168c6a008f81bc1
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-15 09:56:06 +0100

    Create dummy tests and empty bank account classes

--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -3,9 +3,9 @@ package com.booleanuk.core;
 import java.time.LocalDateTime;
 
 public class Transaction {
-    private double amount;
-    private double currentBalance;
-    private LocalDateTime dateTime;
+    private final double amount;
+    private final double currentBalance;
+    private final LocalDateTime dateTime;
 
     public Transaction(double amount, double currentBalance, LocalDateTime dateTime) {
         this.amount = amount;
---
+++ src/test/java/com/booleanuk/core/TestBankAccount.java
@@ -0,0 +1,35 @@
+package com.booleanuk.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestBankAccount {
+    @Test
+    public void newAccountInstancesAreValid() {
+        assertTrue(true);
+    }
+    @Test
+    public void withdrawTooMuchShouldFail() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void canWithdrawWithinReason() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void depositAddsTheRightAmount() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void generateStatementOnNoTransactions() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void generateStatementOnSomeTransactions() {
+        assertTrue(true);
+    }
+}
